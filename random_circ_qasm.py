0e5a3fb5aab0debe33eb4e184b288783cda44d38
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2023-12-09 13:23:35 +0100

    Add command line arguments
    Former-commit-id: fb01af2461febbfa2161105be220493942f21fe2 [formerly 2e4202e1478c628c840b13a28cb3398f079775c6]
    Former-commit-id: 3296b734bf35ec4095fd0f8610d29de92022c044

--- random_circ_qasm.py
+++ random_circ_qasm.py
@@ -1,4 +1,4 @@
-import random, os, shutil
+import random, os, shutil, sys
 
 def generate_random_qasm_circuit(n, m, t_prob):
     circuit_lines = []
@@ -124,12 +124,25 @@ def DataPoint(n, m, Tprob):
     return folder + '/' + filename
 
 
-def main():
+def main(filename, n, d, r):
     print("")
-    random_circuit = generate_random_qasm_circuit(10000, 30, .2)
+    random_circuit = generate_random_qasm_circuit(n, d, r)
     circuit_content = '\n'.join(random_circuit)
-    WriteFile(".", "random.qasm", circuit_content)
+    WriteFile(".", filename, circuit_content)
 
     
 if __name__ == "__main__":
-    main() 
+    if len(sys.argv) < 2:
+        exit(1)
+
+    qubits = 1000
+    depth  = 30
+    Trate  = .2
+    
+    if len(sys.argv) > 2:
+        qubits = int(sys.argv[2])
+    elif len(sys.argv) > 3:
+        qubits = int(sys.argv[3])
+    elif len(sys.argv) > 4:
+        qubits = float(sys.argv[4])
+    main(sys.argv[1], qubits, depth, Trate) 
