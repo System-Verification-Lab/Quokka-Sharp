eddfc798ff5f16afe49bfd2b4644050eb4bc967f
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2023-12-17 22:59:18 +0100

    Generated Python code

--- cliffordT.py
+++ cliffordT.py
@@ -1,101 +1,110 @@
+# Equivalent(R, r ^ (x[k] & z[k]))
 def H2CNF(tab, cnf, _, k):
-    x = tab.x; z = tab.z; r = tab.r
-
+    r = tab.r
+    x = tab.x
+    z = tab.z
     R = cnf.add_var()
-    cnf.add_clause([ p, -r,  x[k]])
-	cnf.add_clause([ p, -r,  z[k]])
-	cnf.add_clause([-p,  r,  x[k]])
-	cnf.add_clause([-p,  r,  z[k]])
-	cnf.add_clause([ p,  r, -x[k], -z[k]])
-	cnf.add_clause([-p, -r, -x[k], -z[k]])
-    tab.r = p
+    cnf.add_clause([ R, -r,  x[k]])
+    cnf.add_clause([ R, -r,  z[k]])
+    cnf.add_clause([-R,  r,  x[k]])
+    cnf.add_clause([-R,  r,  z[k]])
+    cnf.add_clause([ R,  r, -x[k], -z[k]])
+    cnf.add_clause([-R, -r, -x[k], -z[k]])
+    x[k], z[k] = z[k], x[k]
 
-    x[k], z[k] = z[k], x[k] # swap
-            
+    tab.r = R
 
+# Equivalent(R, r ^ (x[k] & z[k]))
+# Equivalent(Z, x[k] ^ z[k])
 def S2CNF(tab, cnf, _, k):
-    x = tab.x; z = tab.z; r = tab.r
+    r = tab.r
+    x = tab.x
+    z = tab.z
+    R = cnf.add_var()
+    cnf.add_clause([ R, -r,  x[k]])
+    cnf.add_clause([ R, -r,  z[k]])
+    cnf.add_clause([-R,  r,  x[k]])
+    cnf.add_clause([-R,  r,  z[k]])
+    cnf.add_clause([ R,  r, -x[k], -z[k]])
+    cnf.add_clause([-R, -r, -x[k], -z[k]])
 
-    p = cnf.add_var()
-	cnf.add_clause([ p, -r,  x[k]])
-	cnf.add_clause([ p, -r,  z[k]])
-	cnf.add_clause([-p,  r,  x[k]])
-	cnf.add_clause([-p,  r,  z[k]])
-	cnf.add_clause([ p,  r, -x[k], -z[k]])
-	cnf.add_clause([-p, -r, -x[k], -z[k]])
-    # cnf.add_weight(p, -1)
-    # cnf.add_weight(-p, 1)
+    Z = cnf.add_var()
+    cnf.add_clause([ Z,  x[k], -z[k]])
+    cnf.add_clause([ Z, -x[k],  z[k]])
+    cnf.add_clause([-Z,  x[k],  z[k]])
+    cnf.add_clause([-Z, -x[k], -z[k]])
 
-    zp = cnf.add_var() 
-    cnf.add_clause([-zp, -x[k], -z[k]])
-    cnf.add_clause([-zp,  x[k],  z[k]])
-    cnf.add_clause([ zp, -x[k],  z[k]])
-    cnf.add_clause([ zp,  x[k], -z[k]])
+    tab.z[k] = Z
+    tab.r = R
 
-    z[k] = zp
-    tab.r = p
-
-
+# Equivalent(R, r ^ (x[k] & z[k] & ~Z))
+# Equivalent(X, x[k])
+# x[k] | (Equivalent(Z, z[k]))
+# Equivalent(u, x[k])
 def T2CNF(tab, cnf, t, k):
-    x = tab.x; z = tab.z; r = tab.r
-    
-    p = cnf.add_var()
-    zp = cnf.add_var()
-    cnf.add_clause([-r, -p, -x[k], -z[k], zp])
-    cnf.add_clause([-r,  p,  x[k]])
-    cnf.add_clause([-r,  p,  z[k]])
-    cnf.add_clause([-r,  p, -zp])
-    cnf.add_clause([ r, -p,  x[k]])
-    cnf.add_clause([ r, -p,  z[k]])
-    cnf.add_clause([ r, -p, -zp])
-    cnf.add_clause([ r,  p, -x[k], -z[k], zp])
+    r = tab.r
+    x = tab.x
+    z = tab.z
+    R = cnf.add_var()
+    Z = cnf.add_var()
+    X = cnf.add_var()
+    cnf.add_clause([ R, -r,  x[k]])
+    cnf.add_clause([ R, -r,  z[k]])
+    cnf.add_clause([-R,  r,  x[k]])
+    cnf.add_clause([-R,  r,  z[k]])
+    cnf.add_clause([ R, -Z, -r])
+    cnf.add_clause([-R, -Z,  r])
+    cnf.add_clause([ R,  Z,  r, -x[k], -z[k]])
+    cnf.add_clause([-R,  Z, -r, -x[k], -z[k]])
 
-    cnf.add_clause([x[k], -zp,  z[k]])
-    cnf.add_clause([x[k],  zp, -z[k]])
-    # u = x[k]                               # this was a useless assignment?
+    cnf.add_clause([ X, -x[k]])
+    cnf.add_clause([-X,  x[k]])
+
+    cnf.add_clause([ Z,  x[k], -z[k]])
+    cnf.add_clause([-Z,  x[k],  z[k]])
+
     u = cnf.add_var()
-    cnf.add_clause([-u, x[k]])            # TODO: note that we can actually eliminate u and assign a weight to x[k] instead...
-    cnf.add_clause([u, -x[k]])
-    cnf.add_weight(u, 0.707106781186548)    # TODO: is this enough significance?
-                                            # TODO: Perhaps sanity check robustness of WMC by varying significance..
-    # 0.7071067
-    cnf.add_weight(-u, 1)
+    cnf.add_clause([ u, -x[k]])
+    cnf.add_clause([-u,  x[k]])
+    cnf.add_weight(u, 0.707106781186548)
 
-    z[k] = zp
-    tab.r = p
+    tab.x[k] = X
+    tab.z[k] = Z
+    tab.r = R
 
-
+# Equivalent(R, r ^ (x[c] & z[t] & (z[c] ^ ~x[t])))
+# Equivalent(X, x[c] ^ x[t])
+# Equivalent(Z, z[c] ^ z[t])
 def CNOT2CNF(tab, cnf, _, c, t):
-    x = tab.x; z = tab.z; r = tab.r
+    r = tab.r
+    x = tab.x
+    z = tab.z
+    R = cnf.add_var()
+    Z = cnf.add_var()
+    X = cnf.add_var()
+    cnf.add_clause([ R, -r,  x[c]])
+    cnf.add_clause([ R, -r,  z[t]])
+    cnf.add_clause([-R,  r,  x[c]])
+    cnf.add_clause([-R,  r,  z[t]])
+    cnf.add_clause([ R, -r,  x[t], -z[c]])
+    cnf.add_clause([ R, -r, -x[t],  z[c]])
+    cnf.add_clause([-R,  r,  x[t], -z[c]])
+    cnf.add_clause([-R,  r, -x[t],  z[c]])
+    cnf.add_clause([ R,  r, -x[c],  x[t],  z[c], -z[t]])
+    cnf.add_clause([ R,  r, -x[c], -x[t], -z[c], -z[t]])
+    cnf.add_clause([-R, -r, -x[c],  x[t],  z[c], -z[t]])
+    cnf.add_clause([-R, -r, -x[c], -x[t], -z[c], -z[t]])
 
-    p = cnf.add_var()
-	cnf.add_clause([ p, -r,  x[j]])
-	cnf.add_clause([ p, -r,  z[k]])
-	cnf.add_clause([-p,  r,  x[j]])
-	cnf.add_clause([-p,  r,  z[k]])
-	cnf.add_clause([ p, -r,  x[k],  z[j]])
-	cnf.add_clause([-p,  r,  x[k],  z[j]])
-	cnf.add_clause([ p, -r, -x[k], -z[j]])
-	cnf.add_clause([-p,  r, -x[k], -z[j]])
-	cnf.add_clause([ p,  r, -x[j],  x[k], -z[j], -z[k]])
-	cnf.add_clause([ p,  r, -x[j], -x[k],  z[j], -z[k]])
-	cnf.add_clause([-p, -r, -x[j],  x[k], -z[j], -z[k]])
-	cnf.add_clause([-p, -r, -x[j], -x[k],  z[j], -z[k]])
-    # cnf.add_weight(p, -1)
-    # cnf.add_weight(-p, 1)
+    cnf.add_clause([ X,  x[c], -x[t]])
+    cnf.add_clause([ X, -x[c],  x[t]])
+    cnf.add_clause([-X,  x[c],  x[t]])
+    cnf.add_clause([-X, -x[c], -x[t]])
 
-    xp = cnf.add_var()
-    cnf.add_clause([-x[j], -x[k], -xp])
-    cnf.add_clause([-x[j],  x[k],  xp])
-    cnf.add_clause([ x[j], -x[k],  xp])
-    cnf.add_clause([ x[j],  x[k], -xp])
-    
-    zp = cnf.add_var()   
-    cnf.add_clause([-z[j], -z[k], -zp])
-    cnf.add_clause([-z[j],  z[k],  zp])
-    cnf.add_clause([ z[j], -z[k],  zp])
-    cnf.add_clause([ z[j],  z[k], -zp])
+    cnf.add_clause([ Z,  z[c], -z[t]])
+    cnf.add_clause([ Z, -z[c],  z[t]])
+    cnf.add_clause([-Z,  z[c],  z[t]])
+    cnf.add_clause([-Z, -z[c], -z[t]])
+    tab.x[t] = X
+    tab.z[c] = Z
+    tab.r = R
 
-    tab.r = p
-    x[k] = xp
-    z[j] = zp
