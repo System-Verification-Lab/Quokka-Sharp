b785b3293ae5ea011be4b393b89d4e44bc996055
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-01-13 21:26:26 +0800

    Support allzero measurement
    Former-commit-id: 481f9fea159f7cfd14f84aea755cc9bd4187cb97 [formerly 497633021de86e6d6043d33f9a382edd3b0c09e2]
    Former-commit-id: ad1490b5c69d64478273c529adeb8523b4da88d2

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 
-QuiZX_PATH = "/Users/meij/quizx/quizx/target/debug/measure_test"
+QuiZX_PATH_single = "/Users/meij/quizx/quizx/target/debug/measure_single"
+QuiZX_PATH_multi = "/Users/meij/quizx/quizx/target/debug/measure_multi"
 GPMC_PATH = "/Users/meij/Desktop/qc2cnf/tools/GPMC"
 TIMEOUT = 300
