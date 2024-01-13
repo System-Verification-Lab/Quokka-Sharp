497633021de86e6d6043d33f9a382edd3b0c09e2
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-01-13 21:26:26 +0800

    Support allzero measurement

--- measure.py
+++ measure.py
@@ -1,11 +1,19 @@
-def M2CNF(tab, cnf):
+def M2CNF(tab, cnf, multi_or_single):
     n = tab.n; x = tab.x; z = tab.z; r = tab.r
 
     cnf.add_weight( r, -1)
     cnf.add_weight(-r,  1)
-    for i in range(n):
-        cnf.add_clause([-x[i]])
-        if i == 0:
-            cnf.add_clause([z[i]])
-        else:
-            cnf.add_clause([-z[i]])
+    if multi_or_single == "multi":
+        for i in range(n):
+            cnf.add_clause([-x[i]])
+            # if i == 0:
+            #     cnf.add_clause([z[i]])
+            # else:
+            #     cnf.add_clause([-z[i]])
+    else:
+        for i in range(n):
+            cnf.add_clause([-x[i]])
+            if i == 0:
+                cnf.add_clause([z[i]])
+            else:
+                cnf.add_clause([-z[i]])
