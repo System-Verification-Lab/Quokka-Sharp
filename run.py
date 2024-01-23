e55720e1b2155c568f938fad11449f35b0bb39fc
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-01-23 20:45:16 +0100

    Update run.py
    Former-commit-id: 6e2111fbaf3125e1bd6a1da40d623802731c0144 [formerly 17429a5090733a93020727ea64c01a66eb724172]
    Former-commit-id: e5ee00bff8f8cba2f9b20f95473ddd5dde6eba42

--- run.py
+++ run.py
@@ -26,12 +26,13 @@ def QC2SAT(qasm_file ,multi_or_single):
     os.mkdir(GPMC_PATH + '/example/' + folder)
     wmc_file = GPMC_PATH + '/example/'+ folder + '/' + filename
     prep_start = time.time()
-    os.system('python3 qasm2cnf.py ' + qasm_file + ' ' + wmc_file + ' ' + multi_or_single)
+    info = os.popen('python3 qasm2cnf.py ' + qasm_file + ' ' + wmc_file + ' ' + multi_or_single).read()
     prep_end = time.time()
     t_prep = round((prep_end - prep_start) * 1000, 3)
-    return t_prep
+    info = re.split(",|\[|\]",info)
+    return [t_prep, int(info[1])]
 
-def GPMC(filename):
+def GPMC(filename, n, multi_or_single):
     filepath = filename.split('/')
     l = len(filepath)
     filepath2 = filepath[l-3] + "/" + filepath[l-2] + "/" + filepath[l-1]
@@ -45,14 +46,21 @@ def GPMC(filename):
         result = str(result)
         gpmc_time_str = re.findall(r"Real.time.*s",str(result))[0]
         gpmc_time = round(float(re.findall(r"[-+]?(?:\d*\.*\d+)", gpmc_time_str)[0]) * 1000, 3)
-        gpmc_ans_str = re.findall(r"exact.*\\nc s",result)[0]
+        gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
+        # print(gpmc_ans_str)
         if "e-" in gpmc_ans_str == 2: # deal with exact: -8.72889813224858e-09
-            gpmc_ans_str = '0'
-        print(gpmc_ans_str)
-        mem_str = re.findall(r"Memory.used.*", str(result))[0]
-        gpmc_ans = (float(re.findall(r"[-+]?(?:\d*\.*\d+)", gpmc_ans_str)[0]))
-        print("The resulting probability by GPMC is " + str(gpmc_ans/2 + 1/2))
-        print("The running time of GPMC is " + str(gpmc_time) + "ms" + " " + mem_str)
+            gpmc_ans = 0
+        elif "e+" in gpmc_ans_str:
+            gpmc_ans = float(gpmc_ans_str)
+        # mem_str = re.findall(r"Memory.used.*", str(result))[0]
+        else: 
+            gpmc_ans = (float(re.findall(r"[-+]?(?:\d*\.*\d+)", gpmc_ans_str)[0]))
+        if multi_or_single == "multi":
+            print("gpmc prop = " + str(gpmc_ans / math.pow(2,int(n))))            
+        else:
+            print("gpmc prop = " + str(gpmc_ans/2+1/2))
+            
+        # print("The running time of GPMC is " + str(gpmc_time) + "ms" + " " + mem_str)
         return gpmc_time
     except TimeoutExpired:
         p.kill() 
@@ -67,12 +75,10 @@ def ZX(filename, multi_or_single):
     try:
         result, err = p.communicate(timeout=TIMEOUT)
         result = str(result)
-        print(result, err)
         result_str = re.findall(r"re\(P\).*$",result)[0]
         print(result_str)
         zx_time_str = re.findall(r"tall.*$",result)[0]
         zx_time = re.findall(r"[-+]?(?:\d*\.*\d+)", zx_time_str)[0]
-        # print(zx_time_str)
         if "ms" in zx_time_str:
             return float(zx_time)
         else:
@@ -88,20 +94,23 @@ def main(folder, multi_or_single):
     gpmc_time_list = []
     succ_wmc = 0
     succ_zx = 0
-    print(multi_or_single)
     for qasm_file in circuitlist:
         print(qasm_file)
         zx_time = ZX(qasm_file, multi_or_single)
         zx_time_list.append(zx_time)
+        print("zx time =" + str(zx_time))
         if zx_time < TIMEOUT * 1000:
             succ_zx += 1
-        pre_time = QC2SAT(qasm_file, multi_or_single)
-        gpmc_time = GPMC(qasm_file)
+        pre_time, n = QC2SAT(qasm_file, multi_or_single)
+        gpmc_time = GPMC(qasm_file, n, multi_or_single)
+        print("gpmc time=" + str(pre_time + gpmc_time))
         if gpmc_time == TIMEOUT * 1000: pre_time = 0
         gpmc_time_list.append(gpmc_time + pre_time)
         if gpmc_time < TIMEOUT * 1000:
             succ_wmc += 1
-    # print("succ_wmc: " + str(succ_wmc / 50.0) + " succ_zx: " + str(succ_zx / 50.0) + " min_wmc: " + str(min(gpmc_time_list)) + " min_zx: " + str(min(zx_time_list)))
+    zx_time_list.append(1000000)
+    gpmc_time_list.append(1000000)
+    print("succ_wmc: " + str(succ_wmc / 50.0) + " succ_zx: " + str(succ_zx / 50.0) + " min_wmc: " + str(min(gpmc_time_list)) + " min_zx: " + str(min(zx_time_list)))
 
 
 if __name__ == "__main__":
