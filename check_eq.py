2eaa8ed8a94d44d1df8063295a2226384fadb23f
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-01-31 17:19:54 +0100

    add literal to the beginning of conslist

--- check_eq.py
+++ check_eq.py
@@ -2,7 +2,9 @@ import re, os, sys, copy
 from encoding.Tableau import Tableau, CNF
 from encoding.qc2cnf_eq import qc2cnf
 from settings import GPMC_PATH
-
+import time
+from queue import Queue
+from memory import ReturnValueThread, memory_monitor
 
 def EC2CNF(tab, cnf, Z_or_X, idx, init):
     x = tab.x; z = tab.z; r = tab.r; n = tab.n
@@ -11,24 +13,25 @@ def EC2CNF(tab, cnf, Z_or_X, idx, init):
         cnf.add_weight(-r,  1)
     if Z_or_X == "Z":
         for i in range(n):
-            cnf.add_clause([-x[i]])
+            cnf.insert_clause([-x[i]])
             if i == idx:
-                cnf.add_clause([z[i]])
+                cnf.insert_clause([z[i]])
             else:
-                cnf.add_clause([-z[i]])
+                cnf.insert_clause([-z[i]])
     else:
         for i in range(n):
-            cnf.add_clause([-z[i]])
+            cnf.insert_clause([-z[i]])
             if i == idx:
-                cnf.add_clause([x[i]])
+                cnf.insert_clause([x[i]])
             else:
-                cnf.add_clause([-x[i]])
+                cnf.insert_clause([-x[i]])
 
 def write_cnf(cnf, cnf_file):
     with open(cnf_file, 'w') as the_file:
         the_file.writelines("p cnf " + str(cnf.var)+" "+str(cnf.clause)+"\n")
         the_file.write(cnf.weight_list.getvalue())
-        the_file.write(cnf.cons_list.getvalue())
+        for item in cnf.cons_list:
+            the_file.writelines(item)
 
 def GPMC(cnf_file):
     gpmc_path = GPMC_PATH + '/bin/gpmc'
@@ -42,20 +45,21 @@ def GPMC(cnf_file):
     print(gpmc_time, gpmc_ans)
     return gpmc_time, gpmc_ans
 
-def main(qasmfile1, qasmfile2, cnf_file):
+def checker(qasmfile1, qasmfile2, cnf_file):
     tab, cnf, tab_init = qc2cnf(qasmfile1, qasmfile2)
     
     n = tab.n
     sum_time = 0
+
     for i in range(n):
         cnf_temp = copy.deepcopy(cnf)
-        EC2CNF(tab_init, cnf_temp, 'Z', i, True)
         EC2CNF(tab, cnf_temp, 'Z', i, False)
+        EC2CNF(tab_init, cnf_temp, 'Z', i, True)
         write_cnf(cnf_temp, cnf_file)
         time, prob = GPMC(cnf_file)
         sum_time += time
         if abs(prob - 1) > 1e-10:
-            return False, sum_time
+            return False
     for i in range(n):
         cnf_temp = copy.deepcopy(cnf)
         EC2CNF(tab_init, cnf_temp, 'X', i, True)
@@ -63,8 +67,26 @@ def main(qasmfile1, qasmfile2, cnf_file):
         write_cnf(cnf_temp, cnf_file)
         time, prob = GPMC(cnf_file)
         if abs(prob - 1) > 1e-10:
-            return False, sum_time
-    return True, sum_time
+            return False
+    return True
 
+def main(qasmfile1, qasmfile2, cnf_file):
+    queue = Queue()
+    poll_interval = 0.1
+    monitor_thread = ReturnValueThread(target=memory_monitor, args=(queue, poll_interval))
+    monitor_thread.start()
+    # wait a bit for monitor thread to start measuring mem
+    time.sleep(.5)
+    
+    start = time.time()
+    result = checker(qasmfile1, qasmfile2, cnf_file)
+    end = time.time()
+
+    queue.put('stop')
+    max_rss = monitor_thread.join()
+    print(  ' time:', (end - start),
+            ' result:', result,
+            ' Max RSS:', max_rss / 1024 / 1024, "MB")
+    
 if __name__ == '__main__':
-    print(main(sys.argv[1], sys.argv[2], sys.argv[3]))
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
--- encoding/Tableau.py
+++ encoding/Tableau.py
@@ -13,7 +13,8 @@ class CNF:
     def __init__(self):
         self.var = 0
         self.clause = 0
-        self.cons_list = io.StringIO()
+        # self.cons_list = io.StringIO()
+        self.cons_list = []
         self.weight_list = io.StringIO()
     
     def add_var(self):
@@ -22,10 +23,22 @@ class CNF:
     
     def add_clause(self, cons):
         self.clause += 1
+        constr = ''
         for i in range(len(cons)):
-            self.cons_list.write(str(cons[i]))
-            self.cons_list.write(" ")
-        self.cons_list.write("0\n")
+            constr += str(cons[i]) + " "
+            # self.cons_list.write(str(cons[i]))
+            # self.cons_list.write(" ")       
+        # self.cons_list.write("0\n")
+        constr = constr + "0\n"
+        self.cons_list.append(constr)
+    
+    def insert_clause(self, cons):
+        self.clause += 1
+        constr = ''
+        for i in range(len(cons)):
+            constr += str(cons[i]) + " "
+        constr = constr + "0\n"
+        self.cons_list.insert(0, constr)       
     
     def add_weight(self, var, weight):
         self.weight_list.write("c p weight ")
