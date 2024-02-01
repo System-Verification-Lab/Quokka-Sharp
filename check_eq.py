c02b9f8b2409f77fa59fb7a0f37d68255cc7aeb3
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-02-01 23:17:40 +0100

    parrallel

--- check_eq.py
+++ check_eq.py
@@ -1,17 +1,24 @@
 import re, os, sys, copy
-from encoding.Tableau import Tableau, CNF
 from encoding.qc2cnf_eq import qc2cnf
 from settings import GPMC_PATH
 import time
+from multiprocessing import Pool
 from queue import Queue
 from memory import ReturnValueThread, memory_monitor
+
+global qasmfile1, qasmfile2
+global tab, cnf, tab_init
+qasmfile1 = sys.argv[1]
+qasmfile2 = sys.argv[2]
+tab, cnf, tab_init = qc2cnf(qasmfile1, qasmfile2)
 
 def EC2CNF(tab, cnf, Z_or_X, idx, init):
     x = tab.x; z = tab.z; r = tab.r; n = tab.n
     if not init:
         cnf.add_weight( r, -1)
         cnf.add_weight(-r,  1)
-    if Z_or_X == "Z":
+        # When basis is Z
+    if Z_or_X == True:    
         for i in range(n):
             cnf.insert_clause([-x[i]])
             if i == idx:
@@ -42,44 +49,48 @@ def GPMC(cnf_file):
     gpmc_ans = float(gpmc_ans_str)
     if abs(gpmc_ans) < 0.00000001:
         gpmc_ans = 0
-    print(gpmc_time, gpmc_ans)
-    return gpmc_time, gpmc_ans
+    return gpmc_ans
 
-def checker(qasmfile1, qasmfile2, cnf_file):
-    tab, cnf, tab_init = qc2cnf(qasmfile1, qasmfile2)
-    
-    n = tab.n
-    sum_time = 0
+def checker(i, Z_or_X, cnf_file):
+    cnf_temp = copy.deepcopy(cnf)
+    EC2CNF(tab, cnf_temp, Z_or_X, i, False)
+    EC2CNF(tab_init, cnf_temp, Z_or_X, i, True)
+    write_cnf(cnf_temp, cnf_file)
+    prob = GPMC(cnf_file)
+    if abs(prob - 1) > 1e-10:
+        return False
+    else: return True
 
-    for i in range(n):
-        cnf_temp = copy.deepcopy(cnf)
-        EC2CNF(tab, cnf_temp, 'Z', i, False)
-        EC2CNF(tab_init, cnf_temp, 'Z', i, True)
-        write_cnf(cnf_temp, cnf_file)
-        time, prob = GPMC(cnf_file)
-        sum_time += time
-        if abs(prob - 1) > 1e-10:
-            return False
-    for i in range(n):
-        cnf_temp = copy.deepcopy(cnf)
-        EC2CNF(tab_init, cnf_temp, 'X', i, True)
-        EC2CNF(tab, cnf_temp, 'X', i, False)
-        write_cnf(cnf_temp, cnf_file)
-        time, prob = GPMC(cnf_file)
-        if abs(prob - 1) > 1e-10:
-            return False
-    return True
-
-def main(qasmfile1, qasmfile2, cnf_file):
+def main():
     queue = Queue()
     poll_interval = 0.1
     monitor_thread = ReturnValueThread(target=memory_monitor, args=(queue, poll_interval))
     monitor_thread.start()
     # wait a bit for monitor thread to start measuring mem
     time.sleep(.5)
-    
+    result = True
+    argu = []
+    for i in range(tab.n):
+        argu.append((i, True, "./cnf/checkerZ" + str(i) + ".cnf"))
+        argu.append((i, False, "./cnf/checkerX" + str(i) + ".cnf"))
+
     start = time.time()
-    result = checker(qasmfile1, qasmfile2, cnf_file)
+    # with Pool(8) as pool:
+    #     for res in pool.starmap(checker, argu):
+    #         if res == False:
+    #             result = res
+    #             pool.terminate()
+    #             break
+    
+    for i in range(tab.n):
+        if not checker(i, True, "./cnf/checkerZ" + str(i) + ".cnf"):
+            result = False
+            break
+        if not checker(i, False, "./cnf/checkerX" + str(i) + ".cnf"):
+            result = False
+            break
+        
+    # result = checker(qasmfile1, qasmfile2, cnf_file)
     end = time.time()
 
     queue.put('stop')
@@ -88,5 +99,5 @@ def main(qasmfile1, qasmfile2, cnf_file):
             ' result:', result,
             ' Max RSS:', max_rss / 1024 / 1024, "MB")
     
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+if __name__ == '__main__':    
+    main()
