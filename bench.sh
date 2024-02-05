81a6cf8d655dae1883c2cadf4ed701312453e25d
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-02-05 15:31:47 +0100

    update cz encoding

--- bench.sh
+++ bench.sh
@@ -4,14 +4,14 @@
 #export PYTHONPATH=$HOME/Projects/Quasimodo/python_pkg:$PYTHON_PATH # for libquasimodo.so
 #export DYLD_LIBRARY_PATH=$HOME/Projects/Quasimodo/python_pkg:$DYLD_LIBRARY_PATH #for python package
 
-for file in ./benchmark/algorithm/eq_bench/origin/*
+for file in ./benchmark/random/randqubitscale/origin/*
 do
     base_name=$(basename ${file})
     base_name="${base_name%.*}"
     echo "$base_name"
     echo GPMC:
-    gtimeout 10 python3 check_eq.py "$file" ./benchmark/algorithm/eq_bench/opt/"$base_name".qasm.opt.qasm
-    echo QCEC:
-    gtimeout 10 python3 qcec.py "$file" ./benchmark/algorithm/eq_bench/opt/"$base_name".qasm.opt.qasm
-    echo
+    gtimeout 10 python3 check_eq.py "$file" ./benchmark/random/randqubitscale/opt/"$base_name".qasm.opt.qasm
+#    echo QCEC:
+#    gtimeout 10 python3 qcec.py "$file" ./benchmark/algorithm/eq_bench/opt/"$base_name".qasm.opt.qasm
+#    echo
 done
