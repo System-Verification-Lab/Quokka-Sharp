f7ea3d9c6a6f5cec9c738f3d4b980ca04ad10681
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2024-02-14 12:34:26 +0100

    Fix GPMC path
    Former-commit-id: 8d6278fe308b488a29340e80f582c90748aff7e1 [formerly 2c8eff65639e001e4d365fec316868d66cca8e69]
    Former-commit-id: ee220c6a5cf3ae57e4c65f65c6db0f6d1effdfc2

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 
 QuiZX_PATH_single = "/Users/meij/quizx/quizx/target/debug/measure_single"
 QuiZX_PATH_multi = "/Users/meij/quizx/quizx/target/debug/measure_multi"
-GPMC_PATH = "/Users/meij/Desktop/qc2cnf/tools/GPMC"
+GPMC_PATH = ""
 TIMEOUT = 300
