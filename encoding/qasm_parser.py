6833920c4fcf82105fdfa12b0dedd36ea728dbe7
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-02-15 13:10:46 +0100

    parse rotation gates
    Former-commit-id: 011a84f3d8f3b95c7a236734dfca49f08857fbd9 [formerly df49d4638778883c8512c6d34050de47fb66514b]
    Former-commit-id: e1f1380a41b01c1f7f8f0dc4a2e1c7c14b5303dc

--- encoding/qasm_parser.py
+++ encoding/qasm_parser.py
@@ -1,10 +1,17 @@
+import re
+
+HermiGates = ['h', 'cx', 'cz', 'x', 'z', 'y', 'ccx']
+RotationGates = ['rx', 'ry', 'rz']
+NHermitGates = {'t': 'tdg', 'tdg': 't', 's': 'sdg', 'sdg': 's', 
+                'rx': 'rxdg', 'rx': 'rxdg', 'rz': 'rzdg', 'rzdg': 'rz', 'ry': 'rydg', 'rydg': 'ry'}
+
 class Circuit:
     def __init__(self, translate_ccx):
         self.n = 0
         self.tgate = 0
         self.circ = []
         self.translate_ccx = translate_ccx
-
+    
     def depth(self):
         return len(self.circ)
     
@@ -15,6 +22,9 @@ class Circuit:
 
     def add_double(self, gate, qubitc,qubitr):
         self.circ.append([gate, qubitc, qubitr])
+    
+    def add_rotation(self, gate, angle, qubit):
+        self.circ.append([gate, angle, qubit])
     
     def add_ccx(self,qubitc1,qubitc2,qubitr):
         if self.translate_ccx:
@@ -44,32 +54,14 @@ class Circuit:
 
     def dagger(self):
         self.circ.reverse()
-        for gate in self.circ:
-            if gate[0] == 'tdg':
-                gate[0] = 't'
-            elif gate[0] == 't':
-                gate[0] = 'tdg'
-            elif gate[0] == 's':
-                gate[0] = 'sdg'
-            elif gate[0] == 'sdg':
-                gate[0] = 's'
-            elif gate[0] == 'y':
-                gate[0] = 'ydg'
-            elif gate[0] == 'ccx':
+        for item in self.circ:
+            gate = item[0]
+            if gate in HermiGates:
                 pass
-            elif gate[0] == 'x':
-                pass
-            elif gate[0] == 'z':
-                pass
-            elif gate[0] == 'h':
-                pass
-            elif gate[0] == 'cx':
-                pass
-            elif gate[0] == 'cz':
-                pass
+            elif gate in list(NHermitGates.keys()):
+                gate = NHermitGates[gate]
             else:
                 raise Exception("Gate "+ gate[0] +" dagger not supported.")
-                #TODO: add a "dagger" boolean flag to gates? Ignore for self-
 
     def merge(self, other):
         assert(self.translate_ccx == other.translate_ccx)
@@ -95,7 +87,6 @@ def qasm_parser(filename, translate_ccx) -> Circuit:
     with open(filename,"r") as qasm:
         for line in qasm:
             qasm_list.append(line.rsplit())
-    # qasm_list = qasm_list[2:]
 
     circuit = Circuit(translate_ccx)
     
@@ -120,7 +111,7 @@ def qasm_parser(filename, translate_ccx) -> Circuit:
         elif line[0] == '//' or line[0] == 'OPENQASM' or line[0] == 'include' or line[0] == 'creg':
             continue
 
-        elif gate == 'cx':
+        elif gate == 'cx' or gate == 'cz':
             if(line[1].count('[') == 1):
                 qubitc = get_num(line[1])
                 qubitr = get_num(line[2])
@@ -128,17 +119,7 @@ def qasm_parser(filename, translate_ccx) -> Circuit:
                 qubits = line[1].split(',')
                 qubitc = get_num(qubits[0])
                 qubitr = get_num(qubits[1]) 
-            circuit.add_double('cx',qubitc,qubitr)
-
-        elif gate == 'cz':
-            if(line[1].count('[') == 1):
-                qubitc = get_num(line[1])
-                qubitr = get_num(line[2])
-            else:
-                qubits = line[1].split(',')
-                qubitc = get_num(qubits[0])
-                qubitr = get_num(qubits[1]) 
-            circuit.add_double('cz',qubitc,qubitr)
+            circuit.add_double(gate, qubitc, qubitr)
 
         elif gate == 'z' or gate == 'y' or gate == 'x' or gate == 'h':
             qubit = get_num(line[1])
@@ -178,23 +159,15 @@ def qasm_parser(filename, translate_ccx) -> Circuit:
                 qubitc1 = get_num(qubits[0])
                 qubitc2 = get_num(qubits[1]) 
                 qubitr = get_num(qubits[2])                
-            circuit.add_ccx(qubitc1, qubitc2, qubitr, dagger)
+            circuit.add_ccx(qubitc1, qubitc2, qubitr)
 
-        elif gate.startswith("rx"):
-            #TODO
-            raise Exception(str(gate) + " undefined.")
-
-        elif gate.startswith("ry"):
-            #TODO
-            raise Exception(str(gate) + " undefined.")
-
-        elif gate.startswith("rz"):
-            #TODO
-            raise Exception(str(gate) + " undefined.")
-
-        elif gate == "m":
-            circuit.add_measurement()
-            
+        elif len(gate) > 4 and gate[0:2] in RotationGates:
+            # TODO: gate exception
+            gate_angle = re.findall(r"(.*?)\((.*?)\)",gate)
+            rgate = gate_angle[0]
+            angle = gate_angle[1]
+            qubit = get_num(line[1])
+            circuit.add_rotation(rgate, angle, qubit)
         else:
             gate = line[0]
             raise Exception(str(gate) + " undefined.")
