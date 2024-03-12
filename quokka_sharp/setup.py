461b0640d46f025cef909204c31718d1778b3bcc
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-03-12 12:14:46 +0100

    add version
    Former-commit-id: 6c6f237010e2757449aaefb32f02cf4703efb2ab [formerly 8888952dc79af38ee171aa1459d0723174b549e3]
    Former-commit-id: 4e731058e11d0bbb2fd9724f9376764e158dc123

--- quokka_sharp/setup.py
+++ quokka_sharp/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8'
+VERSION = '0.0.11'
 DESCRIPTION = 'Quokka Sharp'
 LONG_DESCRIPTION = '''
                     Quokka Sharp for simulating and equivalence checking 
