d48fc4fb046ab083d969ca2378370d0f0613e5db
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-03-17 18:15:10 +0100

    output format
    Former-commit-id: 9d2f742610ea6de42dbc67204b66045674e1a564 [formerly 8085734e38f5c2df01f2581be998dfef71be9bd1]
    Former-commit-id: e21945a3844bee1a6c4e0d193c8c0c2d0fd7f1d9

--- quokka_sharp/quokka_sharp/ecmc.py
+++ quokka_sharp/quokka_sharp/ecmc.py
@@ -5,7 +5,6 @@ import tempfile
 import time
 from subprocess import PIPE, Popen
 
-# TODO: add comments for important codes
 # TODO: information for arguments in functions
 
 class Result:
@@ -21,7 +20,7 @@ def GPMC(cnf_file):
     # if gpmc_path == None:
     #     sys.exit("Binary gpmc not found in path.")
     gpmc_path = TOOL_PATH
-    proc = Popen([gpmc_path, "-mode=1", cnf_file], stdout= PIPE, stderr=PIPE)
+    proc = Popen([gpmc_path, "-mode=1 -prec=17", cnf_file], stdout= PIPE, stderr=PIPE)
     # result, error = proc.communicate()
     return proc
 
@@ -29,30 +28,37 @@ def get_result(result):
     result = str(result)
     gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
     gpmc_ans = float(gpmc_ans_str)
-    if abs(gpmc_ans - 1) > 1e-14:
+    if abs(gpmc_ans - 1) > 1e-15:
         return False
     else: return True
 
 def basis(i, Z_or_X, cnf, cnf_file_root):
     cnf_temp = copy.deepcopy(cnf)
-    cnf_temp.leftProjectZXi(Z_or_X, i)
     cnf_temp.rightProjectZXi(Z_or_X, i)
+    cnf_temp.leftProjectZXi(Z_or_X, i)
+    
     cnf_file = cnf_file_root + "/quokka_eq_check_"+ ("Z" if Z_or_X else "X") + str(i) + ".cnf"
     cnf_temp.write_to_file(cnf_file)
     return cnf_file
 
-def EQ2CNF(cnf, cnf_file_root = tempfile.gettempdir()):
-    argulist = []
-    cnf_file_list = []
-    for i in range(cnf.n):
-        argulist.append((i, True,  cnf))
-        argulist.append((i, False, cnf))   
-    for argu in argulist:
-        cnf_file = basis(argu[0], argu[1], argu[2], cnf_file_root)
-        cnf_file_list.append(cnf_file)
-    return cnf_file_list
-
 def EQ_check(toolpath, cnf_file_list):
+    global TOOL_PATH
+    TOOL_PATH = toolpath
+    #TODO: different number of qubits
+    
+    result = True
+    
+    for file in cnf_file_list:
+        p = GPMC(file)
+        res = p.communicate()
+        result = get_result(res[0])
+        if result == False:
+            break
+    
+    res = Result(0, result)
+    return res
+
+def CheckEquivalence(toolpath, cnf_file_list):
     global TOOL_PATH
     TOOL_PATH = toolpath
     #TODO: different number of qubits
@@ -98,5 +104,5 @@ def EQ_check(toolpath, cnf_file_list):
     
     end = time.time()
 
-    res = Result(end - start, result)
-    return res
+    # res = Result(end - start, result)
+    return result
