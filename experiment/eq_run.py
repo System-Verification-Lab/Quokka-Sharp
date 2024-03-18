48cda91b17f5a186769e3c7dc93bcdb076a6dc1e
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-03-18 15:49:05 +0100

    restructure
    Former-commit-id: 0152ab18aa559aafc93608ba738fb80429160770 [formerly 93ac6189ea809f5ce8e8d4bbd563fd96ce3fd7f5]
    Former-commit-id: 15a1fcf1e4a06862e4629ba73bd6e4726829dd2c

---
+++ experiment/eq_run.py
@@ -0,0 +1,29 @@
+import quokka_sharp as qk
+import sys
+
+def main(tool_path, qasmfile1, qasmfile2):
+    
+    # Parse the circuit
+    circuit1 = qk.encoding.QASMparser(qasmfile1, True)
+    # Parse another circuit
+    circuit2 = qk.encoding.QASMparser(qasmfile2, True)
+    # Get (circuit1)^dagger(circuit2)
+    circuit2.dagger()
+    circuit1.append(circuit2)
+    # Get CNF for the merged circuit
+    cnf = qk.encoding.QASM2CNF(circuit1)
+
+    res = qk.CheckEquivalence(tool_path, cnf)
+    
+    
+    print(res)
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        main("/Users/meij/GPMC/bin/gpmc -mode=1", "test.qasm", "test.qasm")
+    else:
+        tool_path = sys.argv[1]
+        circ1 = sys.argv[2]
+        circ2 = sys.argv[3]
+        main(tool_path, circ1, circ2)
---
+++ experiment/sim_run.py
@@ -0,0 +1,21 @@
+import quokka_sharp as qk
+import sys
+
+def main(tool_invocation, qasmfile1):
+    
+    # Parse the circuit
+    circuit1 = qk.encoding.QASMparser(qasmfile1, True)
+    # Encode the circuit
+    cnf = qk.encoding.QASM2CNF(circuit1)
+    cnf.add_measurement("firstzero")
+    cnf.write_to_file("circ.cnf")
+    prob = qk.Simulate(tool_invocation, "circ.cnf")
+    print(prob)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        main("/Users/meij/GPMC/bin/gpmc -mode=1", "test.qasm")
+    else:
+        tool_invocation = sys.argv[1]
+        circ1 = sys.argv[2]
+        main(tool_invocation, circ1)
