ac18e7f5d4b5a79598113ae3569ad9d75be05e81
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2024-04-18 14:00:32 +0200

    timeout

--- quokka_sharp/quokka_sharp/qcmc.py
+++ quokka_sharp/quokka_sharp/qcmc.py
@@ -1,6 +1,5 @@
-import re
-# from .encoding.qasm_parser import QASMparser
-from subprocess import Popen, PIPE
+import re, os, sys
+from subprocess import Popen, PIPE, TimeoutExpired
 
 class Result:
     def __init__(self, time, prob, memory):
@@ -17,14 +16,23 @@ def Measurement(cnf, multi_or_single):
     return cnf
 
 def GPMC(tool_invocation, wmc_file):
+    try:  
+        TIMEOUT = int(os.environ["TIMEOUT"])
+    except KeyError: 
+        print ("Please set the environment variable TIMEOUT")
+        sys.exit(1)
     tool_invocation = tool_invocation.split(' ')
     tool_invocation.append(wmc_file)
-    p = Popen(tool_invocation, stdout= PIPE, stderr=PIPE)
-    result,err = p.communicate()
-    result = str(result)
-    gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
-    gpmc_ans = float(gpmc_ans_str)   
-    return gpmc_ans
+    p = Popen(tool_invocation, stdout=PIPE)
+    try: 
+        result = p.communicate(timeout = TIMEOUT)
+        result = str(result)    
+        gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
+        gpmc_ans = float(gpmc_ans_str) 
+        return gpmc_ans
+    except TimeoutExpired:
+        os.system("kill -9 " + str(p.pid))
+        return "TIMEOUT"
 
 def Simulate(toolpath, wmc_file):
     prob = GPMC(toolpath, wmc_file)
