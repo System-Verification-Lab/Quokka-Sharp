ded41e168fc99997eb059fc14bdf44f34a3952fd
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-05 19:01:49 +0200

    change bench and run files to compare results

--- experiment/sim_run.py
+++ experiment/sim_run.py
@@ -1,22 +1,32 @@
 import quokka_sharp as qk
 import sys
 
-def main(tool_invocation, qasmfile1):
+def main(reg_tool_invocation, com_tool_invocation, qasmfile1):
     
     # Parse the circuit
     circuit1 = qk.encoding.QASMparser(qasmfile1, True)
     # Encode the circuit
     cnf = qk.encoding.QASM2CNF(circuit1)
     cnf.leftProjectAllZero()
-    cnf.add_measurement("firstzero")
+    cnf.add_measurement("allzero")
     cnf.write_to_file("circ.cnf")
-    prob = qk.Simulate(tool_invocation, "circ.cnf")
-    print(prob)
+    rez = qk.Simulate(reg_tool_invocation, "circ.cnf")
+    prob = abs(rez)
+
+    cnf = qk.encoding.QASM2COMFCNF(circuit1)
+    cnf.leftProjectAllZero()
+    cnf.add_measurement("allzero")
+    cnf.write_to_file("circ_C.cnf")
+    rez_C = qk.Simulate(com_tool_invocation, "circ_C.cnf")
+    prob_C = abs(rez_C)*abs(rez_C)
+
+    assert ((prob - prob_C) < 1e-15), f"Probs are different: {prob} vs {prob_C} \n\t\t Results are: {rez} vs {rez_C} \n\t\t diff: {prob - prob_C}"
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         main("/Users/meij/GPMC/bin/gpmc -mode=1", "test.qasm")
     else:
-        tool_invocation = sys.argv[1]
-        circ1 = sys.argv[2]
-        main(tool_invocation, circ1)
+        reg_tool_invocation = sys.argv[1]
+        com_tool_invocation = sys.argv[2]
+        circ1 = sys.argv[3]
+        main(reg_tool_invocation, com_tool_invocation, circ1)
