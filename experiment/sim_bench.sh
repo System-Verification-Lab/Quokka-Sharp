72c9694af7579e11dcd2031c46a8d29479b79092
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-05 19:01:49 +0200

    change bench and run files to compare results

--- experiment/sim_bench.sh
+++ experiment/sim_bench.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-tool_path="/Users/meij/GPMC/bin/gpmc -mode=1"
+reg_tool_path="/Users/dekel/Library/CloudStorage/OneDrive-UniversiteitLeiden/Formal_Work/project/GPMC_bin/gpmc_old -mode=1"
+com_tool_path="/Users/dekel/Library/CloudStorage/OneDrive-UniversiteitLeiden/Formal_Work/project/GPMC_bin/gpmc -mode=1"
 path=$PWD/benchmark
+# path=$PWD/shortened_files
 
 dirs=(
 "random/randqubitscale"
@@ -12,9 +14,15 @@ dirs=(
 
 TIMEOUT=300
 
+gtimeout "$TIMEOUT" python3 sim_run.py "$reg_tool_path" "$com_tool_path" "$PWD/shortened_files/basic_tests.txt" || { 
+                    echo "Error: \\n python3 sim_run.py \"$reg_tool_path\" \"$com_tool_path\" \"$file\"";
+                    exit 1;}
+
 for i in "${dirs[@]}"; do
     DIR=$path/"$i"
     for file in $DIR/origin/*; do
-        gtimeout "$TIMEOUT" python3 sim_run.py "$tool_path" "$file"
+        gtimeout "$TIMEOUT" python3 sim_run.py "$reg_tool_path" "$com_tool_path" "$file" || { 
+                    echo "Error: python3 sim_run.py \"$reg_tool_path\" \"$com_tool_path\" \"$file\"";
+                    exit 1;}
     done
 done
