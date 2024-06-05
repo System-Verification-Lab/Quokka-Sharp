449a838f895280c02b1688d787c6bd96f4129667
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-05 19:01:49 +0200

    change bench and run files to compare results

--- experiment/eq_run.py
+++ experiment/eq_run.py
@@ -1,7 +1,7 @@
 import quokka_sharp as qk
 import sys
 
-def main(tool_path, qasmfile1, qasmfile2):
+def main(reg_tool_path, com_tool_path, qasmfile1, qasmfile2):
     
     # Parse the circuit
     circuit1 = qk.encoding.QASMparser(qasmfile1, True)
@@ -12,17 +12,19 @@ def main(tool_path, qasmfile1, qasmfile2):
     circuit1.append(circuit2)
     # Get CNF for the merged circuit
     cnf = qk.encoding.QASM2CNF(circuit1)
-    res = qk.CheckEquivalence(tool_path, cnf)
+    res = qk.CheckEquivalence(reg_tool_path, cnf)
+    cnf_C = qk.encoding.QASM2COMCNF(circuit1)
+    res_C = qk.CheckEquivalence(com_tool_path, cnf_C)
+    assert res == res_C, f"Results are different: {res} vs {res_C}"
     
-    
-    print(res)
     
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         main("/Users/meij/GPMC/bin/gpmc -mode=1", "test.qasm", "test.qasm")
     else:
-        tool_path = sys.argv[1]
-        circ1 = sys.argv[2]
-        circ2 = sys.argv[3]
-        main(tool_path, circ1, circ2)
+        reg_tool_path = sys.argv[1]
+        com_tool_path = sys.argv[2]
+        circ1 = sys.argv[3]
+        circ2 = sys.argv[4]
+        main(reg_tool_path, com_tool_path, circ1, circ2)
