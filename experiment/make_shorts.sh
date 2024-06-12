df28684972c1cdb446e3a23d95c4dc83b3e94698
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-12 23:50:49 +0200

    dinamic length for make short
    Former-commit-id: c6c0c57db45cf4786703b8a1822ef6f30f52a2b9 [formerly c9e5b02b9402c5c00a4b80ddf89537937ac3ec7d]
    Former-commit-id: 355164607e4d79a4832c49b30f5a3167f0075146

--- experiment/make_shorts.sh
+++ experiment/make_shorts.sh
@@ -15,16 +15,17 @@ mkdir -p "$destination_directory"
 
 # Loop through each file in the source directory
 for d in "${dirs[@]}"; do
+    echo "Processing directory: $d"
     for sub_dir in "$source_directory$d"*/; do
+        echo "Processing subdirectory: $sub_dir"
         dirname=$(basename "$sub_dir")/
-        echo "Processing directory: $dirname"
         mkdir -p "$destination_directory$d""$dirname"
         for file in "$sub_dir"*; do
             # Get the filename without the extension
             filename=$(basename "$file")
             # Create the shortened version of the file in the destination directory
             shortened_file="$destination_directory$d""$dirname""short_$filename"
-            head -n 50 "$file" > "$shortened_file"
+            head -n $1 "$file" > "$shortened_file"
         done
     done
 done
