8d6cf763b7897fc64f193256dcfbcb08121cc943
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-12 19:56:00 +0200

    make short
    Former-commit-id: 33112ea0ca0ee5aa2c30ee1a5bc8070f4e08edac [formerly 086464f3e3419637550b3fc747280cf82979c4b2]
    Former-commit-id: 411f0362bdd47edb1ed05b057985d2bb17307c02

--- experiment/make_shorts.sh
+++ experiment/make_shorts.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 # Set the source and destination directory paths
-source_directory="./benchmark/algorithm/origin/"
-destination_directory="./shortened_files/algorithm/origin/"
+source_directory="./benchmark/"
+destination_directory="./shortened_files/"
+
+dirs=(
+"algorithm/"
+"random/randqubitscale/"
+"random/randdepscale/"
+)
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$destination_directory"
 
 # Loop through each file in the source directory
-for file in "$source_directory"/*; do
-    # Get the filename without the extension
-    filename=$(basename "$file" | cut -d. -f1)
-    
-    # Create the shortened version of the file in the destination directory
-    shortened_file="${destination_directory}/${filename}_short.txt"
-    head -n 50 "$file" > "$shortened_file"
+for d in "${dirs[@]}"; do
+    for sub_dir in "$source_directory$d"*/; do
+        dirname=$(basename "$sub_dir")/
+        echo "Processing directory: $dirname"
+        mkdir -p "$destination_directory$d""$dirname"
+        for file in "$sub_dir"*; do
+            # Get the filename without the extension
+            filename=$(basename "$file")
+            # Create the shortened version of the file in the destination directory
+            shortened_file="$destination_directory$d""$dirname""short_$filename"
+            head -n 50 "$file" > "$shortened_file"
+        done
+    done
 done
 
