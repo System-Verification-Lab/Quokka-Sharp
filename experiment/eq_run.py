a6a5e74615d9f9090ebc76decca4f4ec310196d5
Dekel Zeldov <dekelzeldov@gmail.com>
2024-06-17 12:54:57 +0200

    minimal printing

--- experiment/eq_run.py
+++ experiment/eq_run.py
@@ -11,10 +11,8 @@ def main(reg_tool_path, com_tool_path, qasmfile1, qasmfile2):
     circuit2.dagger()
     circuit1.append(circuit2)
     # Get CNF for the merged circuit
-    print("**reg")
     cnf = qk.encoding.QASM2CNF(circuit1)
     res = qk.CheckEquivalence(reg_tool_path, cnf)
-    print("**com")
     cnf_C = qk.encoding.QASM2COMFCNF(circuit1)
     res_C = qk.CheckEquivalence(com_tool_path, cnf_C)
     assert res == res_C, f"Results are different: {res} vs {res_C}"
