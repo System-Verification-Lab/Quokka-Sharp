3f5e29d2786c0a98abb86582ff4215294bf47346
dekel zeldov <dekelzeldov@gmail.com>
2024-07-17 19:16:51 +0200

    syn WIP
    Former-commit-id: ace71de2c53c78d36ddf2299a0c2253446ebb232 [formerly 762d50676c4ff2d960c441dcf7def7457f0cbb9d]
    Former-commit-id: b7bb8e89a2d675793a9458e252549b537baae3ea

--- quokka_sharp/quokka_sharp/scmc.py
+++ quokka_sharp/quokka_sharp/scmc.py
@@ -14,33 +14,48 @@ getcontext().prec = 32
 # TODO: information for arguments in functions
 
 def get_result(result, expexted_prob):
+    ''' return (found, weight, assignment)'''
     result = str(result)
-    gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)[0]
-    gpmc_ans_str = gpmc_ans_str.replace("\\n", "").replace(" ", "").replace("i", "j")
-    gpmc_ans = complex(gpmc_ans_str)
-    real, imag = Decimal(gpmc_ans.real), Decimal(gpmc_ans.imag)
-    prob = (abs(real)**2 + abs(imag)**2).sqrt()
-    if abs(prob - expexted_prob) < (expexted_prob+1) * 1e-12:
-        return True
-    else: 
-        return False
+    # get weight
+    weight = re.findall(r"o -?[0-9]+",result)
+    if not weight: 
+        return (False, 0, [])
+    # get assignment
+    assignment = re.findall(r"v [0-9\s\-]+ 0",result)
+    assert assignment
+    assignment = assignment[0].split(" ")
+    assert assignment[0] == "v"
+    assert assignment[-1] == "0"
+    assignment = assignment[1:-1]
+
+    if abs(weight - expexted_prob) < (expexted_prob+1) * 1e-12:
+        return (False, weight, assignment)
+    else:
+        return (True, weight, assignment)
 
 def identity_check(cnf:'CNF', cnf_file_root):
     cnf_temp = copy.deepcopy(cnf)
     cnf_temp.add_identity_clauses()
     
-    cnf_file = cnf_file_root + "/quokka_eq_check_identity.cnf"
-    cnf_temp.write_to_file(cnf_file)
+    cnf_file = cnf_file_root + f"/quokka_syn_{cnf.syn_gate_layer}.cnf"
+    cnf_temp.write_to_file(cnf_file, syntesis_fomat=True)
     return cnf_file
 
-def CheckEquivalence(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gettempdir(), check = "id"):
+def Synthesys(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gettempdir()):
     p = None
+    found = False
+    weight = 0
+    assignment = []
     try:  
         TIMEOUT = int(os.environ["TIMEOUT"])
         class TimeoutException(Exception): pass 
         def timeout(signum, frame):
             if p is not None:
                 p.kill()
+            print(f"""result found: {found} \n
+                  max layers tried: {cnf.syn_gate_layer}
+                  weight achived: {weight} \n
+                  best assignment: {assignment} """)
             raise TimeoutException("TIMEOUT")
     except KeyError: 
         print ("Please set the environment variable TIMEOUT")
@@ -50,19 +65,23 @@ def CheckEquivalence(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gette
         signal.signal(signal.SIGALRM, timeout)
         signal.alarm(TIMEOUT)
 
-        file = identity_check(cnf, cnf_file_root)
         if cnf.computational_basis:
             expected_prob = 2**cnf.n
         else:
             expected_prob = 4**cnf.n
-        command = tool_invocation.split(' ') + [file]
-        p = Popen(command, stdout= PIPE, stderr=PIPE)
-        while True:
-            pid, _ = os.wait()
-            if pid == p.pid:
-                res = p.communicate()
-                result = get_result(res[0], expected_prob)
-                break
-        return result
+
+        while not found:
+            cnf.add_syn_layer()
+            file = identity_check(cnf, cnf_file_root)
+            command = tool_invocation.split(' ') + [file]
+            p = Popen(command, stdout= PIPE, stderr=PIPE)
+            pid = None
+            while pid == p.pid:
+                pid, _ = os.wait()
+            res = p.communicate()
+            found, weight, assignment = get_result(res[0], expected_prob)
+        
+        return cnf.get_syn_cuirct(assignment)
+
     except TimeoutException:
         return "TIMEOUT"
