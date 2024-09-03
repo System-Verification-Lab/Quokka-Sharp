14910d67c5699dfb2c39bb6791e1aa1c19da0b82
dekel zeldov <dekelzeldov@gmail.com>
2024-09-03 15:46:17 +0200

    un-weighted pauli

--- experiment/sim_run.py
+++ experiment/sim_run.py
@@ -8,21 +8,37 @@ def main(tool_invocation, qasmfile1):
     circuit1 = qk.encoding.QASMparser(qasmfile1, True)
     prob = {}
     for mesurement in ["allzero", "firstzero"]:
-        for basis in ["poul", "comp"]:
+        # print()
+        # print(mesurement)
+        for basis in ["pauli", "comp"]:
             # Encode the circuit
-            cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"))
+            cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"), weighted= True)
             cnf.leftProjectAllZero()
             cnf.add_measurement(mesurement)
             prob[basis] = qk.Simulate(tool_invocation, cnf)
-            # print(res[basis])
+            # print(prob[basis])
             if prob[basis] == "TIMEOUT":
                 print("T", end="")
                 return
+            
+            # if basis == "pauli":
+            #     cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"), weighted= False)
+            #     cnf.leftProjectAllZero()
+            #     cnf.add_measurement(mesurement)
+            #     prob_un = qk.Simulate(tool_invocation, cnf)
+            #     # print(res[basis])
+            #     if prob_un == "TIMEOUT":
+            #         print("T", end="")
+            #     else:
+            #         assert abs(prob["pauli"] - prob_un) < 1e-12, f'''
+            #             For mesurement {mesurement} using Pauli w vs uw:
+            #             Probs are different: {prob["pauli"]} vs {prob_un} 
+            #             \n\t\t diff: {prob["pauli"] - prob_un}'''
 
-        assert abs(prob["poul"] - prob["comp"]) < 1e-12, f'''
-            For mesurement {mesurement}:
-            Probs are different: {prob["poul"]} vs {prob["comp"]} 
-            \n\t\t diff: {prob["poul"] - prob["comp"]}'''
+        # assert abs(prob["pauli"] - prob["comp"]) < 1e-12, f'''
+        #     For mesurement {mesurement} using Pauli vs Comp:
+        #     Probs are different: {prob["pauli"]} vs {prob["comp"]} 
+        #     \n\t\t diff: {prob["pauli"] - prob["comp"]}'''
 
 if __name__ == '__main__':
 
@@ -35,8 +51,8 @@ if __name__ == '__main__':
             main(tool_invocation, circ1)
     except AssertionError:
         print(f"""\nAssertion Failed for call:\
-                \n   tool_path = \"{tool_invocation}\"\
-                \n   circ = \"{circ1}\"""")
+                \n   tool_path=\"{tool_invocation}\"\
+                \n   circ=\"{circ1}\"""")
         print()
         print(traceback.format_exc())
         print()
@@ -48,8 +64,8 @@ if __name__ == '__main__':
         print("KeyboardInterrupt")
     except:
         print(f"""\nError for call:\
-                \n   tool_path = \"{tool_invocation}\"\
-                \n   circ = \"{circ1}\"""")
+                \n   tool_path=\"{tool_invocation}\"\
+                \n   circ=\"{circ1}\"""")
         print()
         print(traceback.format_exc())
         print()
