63d4e258e6373eb2e151cb14366661ae8feff2bb
dekel zeldov <dekelzeldov@gmail.com>
2024-09-08 15:06:28 +0200

    unweighted sim

--- experiment/sim_run.py
+++ experiment/sim_run.py
@@ -2,43 +2,49 @@ import quokka_sharp as qk
 import traceback
 import sys
 
-def main(tool_invocation, qasmfile1):
-    
+def main(tool_invocation, qasmfile):
+    DEBUG = False
     # Parse the circuit
-    circuit1 = qk.encoding.QASMparser(qasmfile1, True)
+    circuit = qk.encoding.QASMparser(qasmfile, True)
     prob = {}
     for mesurement in ["allzero", "firstzero"]:
-        # print()
-        # print(mesurement)
-        for basis in ["pauli", "comp"]:
-            # Encode the circuit
-            cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"), weighted= True)
-            cnf.leftProjectAllZero()
-            cnf.add_measurement(mesurement)
-            prob[basis] = qk.Simulate(tool_invocation, cnf)
-            # print(prob[basis])
-            if prob[basis] == "TIMEOUT":
-                print("T", end="")
-                return
-            
-            # if basis == "pauli":
-            #     cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"), weighted= False)
-            #     cnf.leftProjectAllZero()
-            #     cnf.add_measurement(mesurement)
-            #     prob_un = qk.Simulate(tool_invocation, cnf)
-            #     # print(res[basis])
-            #     if prob_un == "TIMEOUT":
-            #         print("T", end="")
-            #     else:
-            #         assert abs(prob["pauli"] - prob_un) < 1e-12, f'''
-            #             For mesurement {mesurement} using Pauli w vs uw:
-            #             Probs are different: {prob["pauli"]} vs {prob_un} 
-            #             \n\t\t diff: {prob["pauli"] - prob_un}'''
+        if DEBUG: print()
+        if DEBUG: print(f"mesurement: {mesurement}")
+        for basis in ["comp", "pauli"]:
+            if DEBUG: print()
+            if DEBUG: print(f"basis: {basis}")
+            for weighted in [True, False]:
+                if DEBUG: print()
+                if DEBUG: print(f"weighted: {weighted}")
+                # Encode the circuit
+                if circuit.has_rotations and not weighted:
+                    prob[basis+str(weighted)] = "ROTATIONS"
+                    print("R", end="")
+                    continue
+                cnf = qk.encoding.QASM2CNF(circuit, computational_basis = (basis == "comp"), weighted= weighted)
+                cnf.leftProjectAllZero()
+                cnf.add_measurement(mesurement)
+                prob[basis+str(weighted)] = qk.Simulate(tool_invocation, cnf)
+                # print(prob[basis])
+                if prob[basis+str(weighted)] == "TIMEOUT":
+                    print("T", end="")
+                else:
+                    print(".", end="")
+            if prob[basis+str(True)] != "TIMEOUT" and prob[basis+str(False)] not in ["TIMEOUT", "ROTATIONS"]:
+                if not abs(prob[basis+str(True)] - prob[basis+str(False)]) < 1e-12:
+                    print(f'''
+                        Diff for file {qasmfile}
+                        For {mesurement} using {basis} with w vs uw: 
+                        {prob[basis+str(True)]} vs {prob[basis+str(False)]} 
+                        ''')
 
-        # assert abs(prob["pauli"] - prob["comp"]) < 1e-12, f'''
-        #     For mesurement {mesurement} using Pauli vs Comp:
-        #     Probs are different: {prob["pauli"]} vs {prob["comp"]} 
-        #     \n\t\t diff: {prob["pauli"] - prob["comp"]}'''
+        if prob["pauli"+str(True)] != "TIMEOUT" and prob["comp"+str(True)]!= "TIMEOUT":
+            if not abs(prob["pauli"+str(True)] - prob["comp"+str(True)]) < 1e-12:
+                print(f'''
+                    Diff for file {qasmfile}
+                    For {mesurement} using Pauli vs Comp (both w): 
+                    {prob["pauli"+str(True)]} vs {prob["comp"+str(True)]} 
+                    ''')
 
 if __name__ == '__main__':
 
--- quokka_sharp/quokka_sharp/qcmc.py
+++ quokka_sharp/quokka_sharp/qcmc.py
@@ -6,7 +6,7 @@ from .encoding.cnf import CNF
 from decimal import Decimal, getcontext
 getcontext().prec = 32
 
-def GPMC(tool_invocation, wmc_file):
+def GPMC(tool_invocation, wmc_file, square):
     try:  
         TIMEOUT = int(os.environ["TIMEOUT"])
     except KeyError: 
@@ -23,8 +23,11 @@ def GPMC(tool_invocation, wmc_file):
         gpmc_ans = complex(gpmc_ans_str)
         real, imag = Decimal(gpmc_ans.real), Decimal(gpmc_ans.imag)
         if abs(real) < 1e-16 and abs(imag) < 1e-16:
-            gpmc_ans = 0
-        return (real*real + imag*imag).sqrt()
+            return 0
+        elif abs(imag) < 1e-16:
+            return real*real if square else real
+        else:
+            return (real*real + imag*imag) if square else (real*real + imag*imag).sqrt()
     except TimeoutExpired:
         os.system("kill -9 " + str(p.pid))
         return "TIMEOUT"
@@ -34,44 +37,43 @@ def Simulate(toolpath, cnf: "CNF"):
     if cnf.weighted:
         filename = "temp_for_sim.cnf"
         cnf.write_to_file(filename)
-        result = GPMC(toolpath, filename)
+        result = GPMC(toolpath, filename, square = cnf.square_result)
         if result != "TIMEOUT":
-            if cnf.square_result:
-                result = result * result
             result = result * (Decimal(1/2)**Decimal(cnf.power_two_normalisation))
         return result
     else:
         sum_results = 0
-        for s in ["p", "m"]:
-            for t in ["e", "o"]:
-                filename = f"temp_for_sim.cnf"
-                if DEBUG: print(filename)
-                cnf_copy = copy.deepcopy(cnf)
-                if s == "p":
-                    cnf_copy.add_clause([-cnf.vars.r])
-                else:
-                    cnf_copy.add_clause([cnf.vars.r])
-                if t == "e":
-                    cnf_copy.add_clause([-cnf.vars.u])
-                else:
-                    cnf_copy.add_clause([cnf.vars.u])
-                
-                cnf_copy.write_to_file(filename)
-                result = GPMC(toolpath, filename)
-                if result == "TIMEOUT":
-                    return result
-                
-                if DEBUG: print(f"initial: {result}")
-                if s == "m":
-                    result = -result
-                
-                if t == "e":
-                    result = result * (Decimal(1/2)**Decimal(cnf.power_two_normalisation))
-                else:
-                    result = result * (Decimal(1/2)**Decimal(cnf.power_two_normalisation-0.5))
+        for c in ["r", "i"] if cnf.computational_basis else ["r"]:
+            complex_sum = 0
+            for s in ["p", "m"]:
+                for t in ["e", "o"]:
+                    filename = f"temp_for_sim.cnf"
+                    if DEBUG: print(c,s,t)
+                    cnf_copy = copy.deepcopy(cnf)
+                    if cnf.computational_basis:
+                        cnf_copy.add_clause([-cnf.vars.i] if (c == "r") else [cnf.vars.i], comment="copmplex")
+                    cnf_copy.add_clause([-cnf.vars.r] if (s == "p") else [cnf.vars.r], comment="sign")
+                    cnf_copy.add_clause([-cnf.vars.u] if (t == "e") else [cnf.vars.u], comment="sqrt2 norm")
+                    
+                    cnf_copy.write_to_file(filename)
+                    result = GPMC(toolpath, filename, square = False)
+                    if result == "TIMEOUT":
+                        return result
+                    
+                    if DEBUG: print(f"   initial: {result}")
+                    if s == "m":
+                        result = -result
+                    
+                    if t == "e":
+                        result = result * (Decimal(1/2)**Decimal(cnf.power_two_normalisation))
+                        if DEBUG: print(f"   fixed: {result} (/{cnf.power_two_normalisation})")
+                    else:
+                        result = result * (Decimal(1/2)**Decimal(cnf.power_two_normalisation-0.5))
+                        if DEBUG: print(f"   fixed: {result} (/{cnf.power_two_normalisation-0.5})")
 
-                sum_results += result
-                if DEBUG: print(f"fixed: {result} (num tgates: {cnf.circuit.tgate})")
-                if DEBUG: print(f"sum_results: {sum_results}")
-                
+                    complex_sum += result
+            sum_results += complex_sum * complex_sum
+        if not cnf.square_result:
+            sum_results = sum_results.sqrt()
+        if DEBUG: print(f"  sum_results: {sum_results}")
         return sum_results
