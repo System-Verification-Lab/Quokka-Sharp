f03499796652e5f3913841f8ec9e370d42de1055
dekel zeldov <dekelzeldov@gmail.com>
2024-10-15 17:19:11 +0200

    other_tools eq

--- experiment/other_tools/sim_run_all.sh
+++ experiment/other_tools/sim_run_all.sh
@@ -1,15 +1,27 @@
-path=/Users/meij/Desktop/coding/cliffordtmc/experiment/benchmark
+path=../experiment
 
 dirs=(
 # "random/randqubitscale"
 # "random/randdepscale" 
 "algorithm"
 )
-export TIMEOUT=5
+
+export TIMEOUT=300
+
+# for file in $path/basic_tests/*; do
+#     python3 sim_run_all.py "$file"
+# done
+
+# for i in "${dirs[@]}"; do
+#     DIR=$path/benchmark/"$i"
+#     for file in $DIR/origin/*; do
+#         python3 sim_run_all.py "$file"
+#     done
+# done
 
 for i in "${dirs[@]}"; do
-    DIR=$path/"$i"
+    DIR=$path/benchmark/"$i"
     for file in $DIR/origin/*; do
-        python3.11 sim_run_all.py "$file"
+        python3 sim_run_all.py "$file"
     done
 done
