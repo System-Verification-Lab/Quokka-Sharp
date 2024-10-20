6cdb6e05943d992a146d382e983fa86c8f7af383
Dekel Zak <dzzak@fs0.das6.cs.vu.nl>
2024-10-21 01:38:25 +0200

    wmc run setup

--- experiment/other_tools/run_eq_wmc.sh
+++ experiment/other_tools/run_eq_wmc.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-export TIMEOUT=6
+export TIMEOUT=$3
 
 # arg1: file with all filepaths to run
 # arg2: basis 'com' or 'pauli'
+# arg3: timeout in seconds (300 = 5 minutes)
 
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.opt.qasm' -b $2 >&1;
+    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.gm.qasm' -b $2 >&1;
+    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.fp.qasm' -b $2 >&1;
+    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.ps4.qasm' -b $2 >&1;
+    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $2 >&1;
 done < "$1"
