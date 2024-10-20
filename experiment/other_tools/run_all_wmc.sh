3cbd83e3bc176b0b764ffca77dd958f22cc41e15
Dekel Zak <dzzak@fs0.das6.cs.vu.nl>
2024-10-21 01:38:25 +0200

    wmc run setup

--- experiment/other_tools/run_all_wmc.sh
+++ experiment/other_tools/run_all_wmc.sh
@@ -2,12 +2,36 @@
 
 mkdir -p results/
 
-# get eq test results for a sample of the benchmarks
-./run_eq_wmc.sh $1 pauli > results/eq_wmc_pauli_results.txt
-./run_eq_wmc.sh $1 comp  > results/eq_wmc_comp_results.txt
+# arg1: file with all filepaths to run
+# arg2: timeout in seconds (300 = 5 minutes)
 
 # get eq test results for a sample of the benchmarks
-./run_sim_wmc.sh $1 allzero pauli   > results/sim_wmc_allzero_pauli_results.txt
-./run_sim_wmc.sh $1 allzero comp    > results/sim_wmc_allzero_comp_results.txt
-./run_sim_wmc.sh $1 firstzero pauli > results/sim_wmc_firstzero_pauli_results.txt
-./run_sim_wmc.sh $1 firstzero comp  > results/sim_wmc_firstzero_comp_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running eq check in pauli basis'
+./run_eq_wmc.sh $1 pauli $2 > results/eq_wmc_pauli_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running eq check in comp basis'
+./run_eq_wmc.sh $1 comp  $2 > results/eq_wmc_comp_results.txt
+
+# get eq test results for a sample of the benchmarks
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running sim check with allzero mesurment in pauli basis'
+./run_sim_wmc.sh $1 allzero pauli   $2 > results/sim_wmc_allzero_pauli_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running sim check with allzero mesurment in comp basis'
+./run_sim_wmc.sh $1 allzero comp    $2 > results/sim_wmc_allzero_comp_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running sim check with firstzero mesurment in pauli basis'
+./run_sim_wmc.sh $1 firstzero pauli $2 > results/sim_wmc_firstzero_pauli_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'running sim check with firstzero mesurment in comp basis'
+./run_sim_wmc.sh $1 firstzero comp  $2 > results/sim_wmc_firstzero_comp_results.txt
+
+date "+%Y-%m-%d %H:%M:%S"
+echo 'done'
