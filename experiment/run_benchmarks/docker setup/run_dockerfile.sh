1d66d94d96aa644b7679f583211613d6c2034a60
Dekel Zak <dzzak@fs0.das6.cs.vu.nl>
2024-10-23 16:06:10 +0200

    docker no chach of pull

--- experiment/run_benchmarks/docker setup/run_dockerfile.sh
+++ experiment/run_benchmarks/docker setup/run_dockerfile.sh
@@ -1,4 +1,4 @@
 docker rm -v -f $(docker ps -qa)
-docker build --tag 'quokka' .
+docker build --build-arg CACHEBUST=$(date +%s) --tag 'quokka' .
 docker run --name quokka_test -t -detach 'quokka'
 docker exec -it quokka_test sh
