a077d67202fdc5394be06f27c7f9ef8c3a07885d
dekel zeldov <dekelzeldov@gmail.com>
2025-04-02 14:50:25 +0200

    syn random bench

--- experiment/syn_run.py
+++ experiment/syn_run.py
@@ -26,7 +26,10 @@ def main(tool_path, qasmfile, eq_tool_path=None):
     helper_folder = "./syn_files/" + qasmfile.split('/')[-1].split('.')[0] + "/"
     if not os.path.exists(helper_folder):
         os.makedirs(helper_folder, exist_ok=True)
-    for comp_basis, onehot, sandwich in [(True, False, False), (False, False, False), (False, True, False)]: #, (False, False, True), (False, True, True)
+    
+    with open(helper_folder+"final_results.txt", "w") as file:
+        file.write(f"basis\tidentity\tres\tlayers\truntime\tweight\tsol_gate_cnt\torg_gate_cnt\n")
+    for comp_basis, onehot, sandwich in [(False, True, False)]: #(True, False, False), (False, False, False), , (False, False, True), (False, True, True)
         glb_st = time.time()
 
         # Parse the circuits
@@ -34,7 +37,7 @@ def main(tool_path, qasmfile, eq_tool_path=None):
         circuit.dagger()
         cnf = qk.encoding.QASM2CNF(circuit, computational_basis = comp_basis, ancillas=0)
 
-        res, weight, sol = qk.Synthesis(tool_path, cnf, cnf_file_root=helper_folder, fidelity_threshold=0.99, bin_search=False, initial_depth=0, onehot_xz = onehot, h_sandwich=sandwich)
+        res, weight, sol, layers = qk.Synthesis(tool_path, cnf, cnf_file_root=helper_folder, fidelity_threshold=1, bin_search=False, initial_depth=0, onehot_xz = onehot, h_sandwich=sandwich)
         
         sol_folder = '/'.join(qasmfile.split('/')[:-2]) + "/" + qasmfile.split('/')[-2]+"_syn_solutions/"
         if not os.path.exists(sol_folder):
@@ -45,27 +48,33 @@ def main(tool_path, qasmfile, eq_tool_path=None):
             file.write(sol)
 
         glb_et = time.time()
-        print(f"\n *** *** ***\t Time: {glb_et-glb_st:.2f} \t Result: {res} \t best weight: {weight} \t", end="")
+        runtime = glb_et - glb_st
+        print(f"\n *** *** ***\t Time: {runtime:.2f}\tResult: {res}\tbest weight: {weight} \t", end="")
 
         if weight:
             sol_depth = qk.encoding.QASMparser(sol_file, True).depth()
-            org_depth = qk.encoding.QASMparser(qasmfile, True).depth()
-            print(f"#ofGates: {qk.encoding.QASMparser(sol_file, True).depth()} (org:{circuit.depth()}) \t")
+        else:
+            sol_depth = None
+        org_depth = qk.encoding.QASMparser(qasmfile, True).depth()
+        print(f"#ofGates: {qk.encoding.QASMparser(sol_file, True).depth()} (org:{circuit.depth()}) \t")
         print(f"\t origin: {qasmfile}")
         print(f"\t solution: {sol_file}")
         print(f"\t ", end='')
         
         if res == "FOUND":
-        #     print(f"FOUND \t weight: {weight:.2f} ", end="")
+        #     print(f"FOUND\tweight: {weight:.2f} ", end="")
             if eq_tool_path is not None:
                 eq_check(eq_tool_path, qasmfile1=qasmfile, qasmfile2=sol_file, expected_res="True",
                 bases = ["paul"], check_types = ["cyclic", "linear", "cyclic_linear", "cyclic_noY"], to_csv=False)
         # elif res == "TIMEOUT":
-        #     print(f"TIMEOUT \t best weight: {weight:.2f}", end="")
+        #     print(f"TIMEOUT\tbest weight: {weight:.2f}", end="")
         # elif res == "CRASH":
-        #     print(f"CRASH \t best weight: {weight:.2f}", end="")
+        #     print(f"CRASH\tbest weight: {weight:.2f}", end="")
         # else:
         #     print(f"ERROR - unrecognised result", end="")
+
+        with open(helper_folder+"final_results.txt", "a") as file:
+            file.write(f"{("comp" if comp_basis else "pauli")}\t{("2n" if onehot else "full")}\t{res}\t{layers}\t{runtime}\t{weight:.5f}\t{sol_depth}\t{org_depth}\n")
 
         print()
         print()
