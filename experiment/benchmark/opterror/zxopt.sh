374cd71fc693ca60093e7b2bc5101ba70843aaed
dekel zeldov <dekelzeldov@gmail.com>
2025-04-09 14:40:11 +0200

    add bench + clean some for SliQEQ
    Former-commit-id: 9a321b00f2e9924ecdc938fca2cdff203556ea24 [formerly f6262aaf16dcbe43e4ee13a3c9f02ac4b2c8794e]
    Former-commit-id: a770f8a9811017dfd62589f6560e60173bed9cdc

--- experiment/benchmark/opterror/zxopt.sh
+++ experiment/benchmark/opterror/zxopt.sh
@@ -5,15 +5,17 @@ path=`dirname $PWD`
 dirs=(
 #"random/randqubitscale"
 #"random/randdepscale"
-#"algorithm"
-"add"
+"algorithm"
+# "add"
 )
 
 for i in "${dirs[@]}"; do
     DIR=$path/"$i"/
     for file in $path/"$i"/origin/*; do
         base_name=$(basename ${file})
-        python3 zxopt.py "$file" "$DIR"/opt/"$base_name".opt.qasm
-        echo $base_name
+        if [ ! -f "$DIR"/opt/"$base_name".opt.qasm ]; then
+            echo $base_name
+            python3 zxopt.py "$file" "$DIR"/opt/"$base_name".opt.qasm
+        fi
     done
 done
