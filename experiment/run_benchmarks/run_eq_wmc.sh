c1de205d9d873d166dde0bd98d8fd9e3e4d9ed8d
dekel zeldov <dekelzeldov@gmail.com>
2025-04-12 23:30:47 +0200

    benchmarks for artifact

--- experiment/run_benchmarks/run_eq_wmc.sh
+++ experiment/run_benchmarks/run_eq_wmc.sh
@@ -7,30 +7,30 @@
 
 export TIMEOUT=$3
 
-echo "results of Tables 2:" >&1
+echo "results of 3 (mid) and 8:" >&1
 while read -r line; do
     python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
-echo "results of Tables 3:" >&1
+echo "results of Table 3 (right) and 7:" >&1
+while read -r line; do
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
+done < "$1"
+
+echo >&1
+
+echo "results of Table 2 (right):" >&1
 while read -r line; do
     python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
-echo "results of Tables 4:" >&1
+echo "results of Table 6:" >&1
 while read -r line; do
     python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $2 >&1;
-done < "$1"
-
-echo >&1
-
-echo "results of Tables 5:" >&1
-while read -r line; do
-    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
