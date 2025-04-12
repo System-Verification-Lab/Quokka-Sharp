2b733d257ec76af50749857449d891e4f54b2f00
dekel zeldov <dekelzeldov@gmail.com>
2025-04-12 17:50:25 +0200

    benchmarks for artifact

--- experiment/run_benchmarks/run_eq_wmc.sh
+++ experiment/run_benchmarks/run_eq_wmc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# This script runs eq_wmc.py on all files listed in the input file.
 # It takes three arguments:
 # arg1: file with all filepaths to run
 # arg2: basis 'comp' or 'pauli'
@@ -10,28 +9,28 @@ export TIMEOUT=$3
 
 echo "results of Tables 2:" >&1
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $2 >&1;
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 echo "results of Tables 3:" >&1
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $2 >&1;
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 echo "results of Tables 4:" >&1
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $2 >&1;
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
 
 echo "results of Tables 5:" >&1
 while read -r line; do
-    python3 './eq_wmc.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
 done < "$1"
 
 echo >&1
