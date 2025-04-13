a29570b98e7879e65f87a278384186573ade702b
dekel zeldov <dekelzeldov@gmail.com>
2025-04-13 15:47:09 +0200

    benchmarks for artifact
    Former-commit-id: a638fd0b6cf7fddad118c252411a74ce4361bec1 [formerly d3317265d29f71f6aace222b510760e632d15128]
    Former-commit-id: 7861d58280af69dc45411b63ef6e33e60002198d

--- experiment/run_benchmarks/run_sim_wmc.sh
+++ experiment/run_benchmarks/run_sim_wmc.sh
@@ -6,6 +6,14 @@
 
 export TIMEOUT=$3
 
+echo "results of Table 2 (left):" >&1
 while read -r line; do
     python3 './run_sim.py' '../benchmark/algorithm/origin/'$line'.qasm' -m $1 -b $2 >&1;
 done < "./benchmarks_list_tab2_sim.txt"
+echo >&1
+
+echo "results of Table 3 (left):" >&1
+while read -r line; do
+    python3 './run_sim.py' '../benchmark/algorithm/origin/'$line'.qasm' -m $1 -b $2 >&1;
+done < "./benchmarks_list_tab3.txt"
+echo >&1
