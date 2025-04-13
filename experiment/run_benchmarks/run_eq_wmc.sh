740ca97d446b7adbe9b2fa1ab0221bd32f58ddd9
dekel zeldov <dekelzeldov@gmail.com>
2025-04-13 14:59:34 +0200

    benchmarks for artifact
    Former-commit-id: c414fa8db2021e95ba49060cacc9fa4543d0a114 [formerly 1561621614999ed1bd725549d63cfdb81537b2b0]
    Former-commit-id: 70677785ee9edc552c7d23c28321a98ba003cdd5

--- experiment/run_benchmarks/run_eq_wmc.sh
+++ experiment/run_benchmarks/run_eq_wmc.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
 # It takes three arguments:
-# arg1: file with all filepaths to run
-# arg2: basis 'comp' or 'pauli'
-# arg3: timeout in seconds (300 = 5 minutes)
+# arg1: basis 'comp' or 'pauli'
+# arg2: timeout in seconds (300 = 5 minutes)
 
-export TIMEOUT=$3
-
-echo "results of 3 (mid) and 8:" >&1
-while read -r line; do
-    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $2 >&1;
-done < "$1"
-
-echo >&1
-
-echo "results of Table 3 (right) and 7:" >&1
-while read -r line; do
-    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $2 >&1;
-done < "$1"
-
-echo >&1
+export TIMEOUT=$2
 
 echo "results of Table 2 (right):" >&1
 while read -r line; do
-    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $2 >&1;
-done < "$1"
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/shift4/'$line'.qasm.ps4.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab2_eq.txt"
+echo >&1
 
+echo "results of Table 3 (mid):" >&1
+while read -r line; do
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab3.txt"
+echo >&1
+
+echo "results of Table 3 (right):" >&1
+while read -r line; do
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab3.txt"
 echo >&1
 
 echo "results of Table 6:" >&1
 while read -r line; do
-    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $2 >&1;
-done < "$1"
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/flip/'$line'.qasm.fp.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab2_eq.txt"
+echo >&1
 
+echo "results of Table 7:" >&1
+while read -r line; do
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab7+8.txt"
+echo >&1
+
+echo "results of Table 8:" >&1
+while read -r line; do
+    python3 './run_eq.py' '../benchmark/algorithm/origin/'$line'.qasm' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' -b $1 >&1;
+done < "./benchmarks_list_tab7+8.txt"
 echo >&1
--- experiment/run_benchmarks/run_sim_wmc.sh
+++ experiment/run_benchmarks/run_sim_wmc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# arg1: file with all filepaths to run
-# arg2: mesurment 'firstzero' or 'allzero'
-# arg3: basis 'comp' or 'pauli'
-# arg4: timeout in seconds (300 = 5 minutes)
+# arg1: mesurment 'firstzero' or 'allzero'
+# arg2: basis 'comp' or 'pauli'
+# arg3: timeout in seconds (300 = 5 minutes)
 
-export TIMEOUT=$4
+export TIMEOUT=$3
 
 while read -r line; do
-    python3 './run_sim.py' '../benchmark/algorithm/origin/'$line'.qasm' -m $2 -b $3 >&1;
-done < "$1"
+    python3 './run_sim.py' '../benchmark/algorithm/origin/'$line'.qasm' -m $1 -b $2 >&1;
+done < "./benchmarks_list_tab2_sim.txt"
