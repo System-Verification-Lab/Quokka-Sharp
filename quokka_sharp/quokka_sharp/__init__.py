e0bc758d844c5fdc4cf9247f628e9f5a092acfc1
dekel zeldov <dekelzeldov@gmail.com>
2025-04-13 18:32:00 +0200

    verification fix

--- quokka_sharp/quokka_sharp/__init__.py
+++ quokka_sharp/quokka_sharp/__init__.py
@@ -3,13 +3,10 @@ __version__ = "0.0.14"
 from .qcmc import Simulate
 from .ecmc import CheckEquivalence
 from .scmc import Synthesis
+from .veri import Verify
+
 from .encoding import CNF
 from .encoding import Variables
 from .encoding import QASMparser
 from .encoding import QASM2CNF
 
-from .extend import ExactEqcheck
-from .extend import Fidelity
-from .extend import ApproxEqcheck
-from .extend import Verify
-from .extend import Ancilla
