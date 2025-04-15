30c71d595f7fe86eab7a4b4881c08223fca2b9fa
dekel zeldov <dekelzeldov@gmail.com>
2025-04-15 11:14:04 +0200

    fix general measurment for sim

--- experiment/run_benchmarks/run_veri_wmc.sh
+++ experiment/run_benchmarks/run_veri_wmc.sh
@@ -4,4 +4,4 @@
 # arg2: timeout in seconds (300 = 5 minutes)
 
 export TIMEOUT=$2
-python3 './run_veri.py' '../benchmark/algorithm/origin/qwalk-v-chain_nativegates_ibm_qiskit_opt0_5.qasm'  -pre {4:0} -post {4:0} -b $1;
+python3 './run_veri.py' '../benchmark/algorithm/origin/qwalk-v-chain_nativegates_ibm_qiskit_opt0_5.qasm'  -pre '{4:0}' -post '{4:0}' -b $1;
