8269190685057633c7ae32b81e9c804ef23a1ddf
dekel zeldov <dekelzeldov@gmail.com>
2025-04-15 11:50:42 +0200

    syn interface

--- experiment/run_benchmarks/run_syn.py
+++ experiment/run_benchmarks/run_syn.py
@@ -1,16 +1,17 @@
+import argparse
 import time
 import quokka_sharp as qk
 import sys
 
-def main(tool_path, qasmfile):
+def main(tool_path, qasmfile, basis, fid):
         glb_st = time.time()
 
         # Parse the circuits
         circuit = qk.encoding.QASMparser(qasmfile, True)
         circuit.dagger()
-        cnf = qk.encoding.QASM2CNF(circuit, computational_basis = False)
+        cnf = qk.encoding.QASM2CNF(circuit, computational_basis = (basis == "comp"))
 
-        res, weight, sol, layers = qk.Synthesis(tool_path, cnf, onehot_xz = True, printing = True)
+        res, weight, sol, layers = qk.Synthesis(tool_path, cnf, onehot_xz = False, printing = True, fidelity_threshold = fid) #((fid == 1) and (basis == "pauli"))
         
         glb_et = time.time()
         runtime = glb_et - glb_st
@@ -34,5 +35,10 @@ def main(tool_path, qasmfile):
 
 if __name__ == '__main__':
     tool_path = "../../../d4v2/build/maxT_static "
-    circ = sys.argv[1]
-    main(tool_path, circ)
+
+    parser = argparse.ArgumentParser(description='Simulating quantum circuits with WMC')
+    parser.add_argument('filename')
+    parser.add_argument('-b', '--basis', choices=['comp', 'pauli'])
+    parser.add_argument('-f', '--fidelity_threshold', type=float, default=1)
+    args = parser.parse_args()
+    main(tool_path, args.filename, args.basis, args.fidelity_threshold)
