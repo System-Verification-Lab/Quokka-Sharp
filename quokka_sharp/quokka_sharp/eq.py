745c78fc7a7276087fc33f1cd63f9ff5e2c8a027
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2025-05-01 18:16:03 +0200

    configure for changing wmc

--- quokka_sharp/quokka_sharp/eq.py
+++ quokka_sharp/quokka_sharp/eq.py
@@ -1,31 +1,28 @@
 import copy
 import os, sys
 import random
-import re
 import tempfile
 from subprocess import PIPE, Popen
 import signal
 
 from .encoding.cnf import CNF
-from decimal import Decimal, getcontext
-getcontext().prec = 32
+from .utils.utils import parse_wmc_result
+from .utils.timeout import timeout, TimeoutException, MemoutError
+from .config import CONFIG
 
-global FPE
-FPE = 1e-12
+from decimal import Decimal, getcontext
+
+# Global constants from config
+DEBUG           = CONFIG["DEBUG"]
+TIMEOUT         = CONFIG["TIMEOUT"]
+tool_invocation = CONFIG["ToolInvocation"]
+get_result      = CONFIG["GetResult"]
+FPE             = CONFIG["FPE"]
+precision       = CONFIG["Precision"]
+
+getcontext().prec = precision
 
 procdict = {}
-
-class TimeoutException(Exception): pass
-
-def timeout(signum, frame):
-    for proc in procdict.values():
-        try:
-            proc.kill()
-        except Exception:
-            pass
-    raise TimeoutException("TIMEOUT")
-
-
 
 # define Python user-defined exceptions
 class InvalidProcessNumException(Exception):
@@ -44,17 +41,9 @@ def get_result(result, expected_prob, abs_value):
     Returns:
         True if the circuits are equivalent otherwise False
     """
-    result = str(result)
-    gpmc_ans_str = re.findall(r"exact.double.prec-sci.(.+?)\\nc s",result)
-    if len(gpmc_ans_str) == 0:
-        return "MEMOUT"
-    gpmc_ans_str = gpmc_ans_str[0].replace("\\n", "").replace(" ", "").replace("i", "j")
-    gpmc_ans = complex(gpmc_ans_str)
-    real, imag = Decimal(gpmc_ans.real), Decimal(gpmc_ans.imag)
-    if abs_value:
-        prob = (abs(real)**2 + abs(imag)**2).sqrt()
-    else:
-        prob = real
+    prob = parse_wmc_result(result, abs_value)
+    if prob == -1:
+        raise MemoutError
     if abs(prob - expected_prob) < (expected_prob * Decimal(FPE)):
         return True
     else:
@@ -97,11 +86,10 @@ def identity_check(cnf:'CNF', cnf_file_root, constrain_2n = False, constrain_no_
     cnf_temp.write_to_file(cnf_file)
     return cnf_file
 
-def CheckEquivalence(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gettempdir(), check = "cyclic", N=16):
+def CheckEquivalence(cnf: 'CNF', cnf_file_root = tempfile.gettempdir(), check = "cyclic", N=16):
     """
     Check if the given circuit is equivalent to the identity
     Args:
-        tool_invocation : the running command of the weighted model counter
         cnf             : the encoded cnf of the given circuit
         cnf_file_root   : the path where the all cnf files are stored.
         check           : types of equivalence checking encoding: either cyclic or linear
@@ -114,106 +102,88 @@ def CheckEquivalence(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gette
     DEBUG = False
     if DEBUG: print()
     if DEBUG: print(f"comp: {cnf.computational_basis}, check: {check}")
-    # try:
-    #     TIMEOUT = int(os.environ["TIMEOUT"])
-    #     class TimeoutException(Exception): pass
-    #     def timeout(signum, frame):
-    #         for pid in procdict.keys():
-    #             procdict[pid].kill()
-    #         raise TimeoutException("TIMEOUT")
-    # except KeyError:
-    #     print ("Please set the environment variable TIMEOUT")
-    #     sys.exit(1)
-    try:
-        TIMEOUT = int(os.environ["TIMEOUT"])
-    except KeyError:
-        print("Please set the environment variable TIMEOUT")
-        sys.exit(1)
 
     try:
-        signal.signal(signal.SIGALRM, timeout)
-        signal.alarm(TIMEOUT)
-
         cnf_file_list = []
         procdict = {}
-
-        if check == "cyclic":
-            if N > 1:
-                raise InvalidProcessNumException
-            cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_2n = False))
-            if cnf.computational_basis:
-                expected_prob = Decimal(2**cnf.n)
+        def cleanup():
+            for p in procdict.values():
+                try:
+                    p.kill()
+                except Exception:
+                    pass
+        with timeout(TIMEOUT, on_timeout=cleanup):
+            if check == "cyclic":
+                if N > 1:
+                    raise InvalidProcessNumException
+                cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_2n = False))
+                if cnf.computational_basis:
+                    expected_prob = Decimal(2**cnf.n)
+                    expected_abs_value = True
+                else:
+                    expected_prob = Decimal(4**cnf.n)
+                    expected_abs_value = False
+            elif check == "cyclic_linear":
+                if N > 1:
+                    raise InvalidProcessNumException
+                cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_2n = True))
+                expected_prob = Decimal(2*cnf.n)
+                expected_abs_value = False
+            elif check == "cyclic_noY":
+                if N > 1:
+                    raise InvalidProcessNumException
+                cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_no_Y = True))
+                expected_prob = Decimal(3**cnf.n)
                 expected_abs_value = True
+            elif check == "linear":
+                if cnf.computational_basis:
+                    assert False, "2n check is not supported for computational basis"
+                else:
+                    for i in range(cnf.n):
+                        cnf_file_list.append(basis(i, True, cnf, cnf_file_root))
+                        cnf_file_list.append(basis(i, False, cnf, cnf_file_root))
+                    expected_prob = 1
+                    expected_abs_value = False
             else:
-                expected_prob = Decimal(4**cnf.n)
-                expected_abs_value = False
-        elif check == "cyclic_linear":
-            if N > 1:
-                raise InvalidProcessNumException
-            cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_2n = True))
-            expected_prob = Decimal(2*cnf.n)
-            expected_abs_value = False
-        elif check == "cyclic_noY":
-            if N > 1:
-                raise InvalidProcessNumException
-            cnf_file_list.append(identity_check(cnf, cnf_file_root, constrain_no_Y = True))
-            expected_prob = Decimal(3**cnf.n)
-            expected_abs_value = True
-        elif check == "linear":
-            if cnf.computational_basis:
-                assert False, "2n check is not supported for computational basis"
-            else:
-                for i in range(cnf.n):
-                    cnf_file_list.append(basis(i, True, cnf, cnf_file_root))
-                    cnf_file_list.append(basis(i, False, cnf, cnf_file_root))
-                expected_prob = 1
-                expected_abs_value = False
-        else:
-            raise ValueError(f"Invalid check type {check}")
-        if DEBUG: print(f"expected: {expected_prob}")
+                raise ValueError(f"Invalid check type {check}")
+            if DEBUG: print(f"expected: {expected_prob}")
 
-        result = True
-        tool_command = tool_invocation.split(' ')
-        # parallel processes
-        # N = 16
-        while True:
-            procdict = {}
-            length = len(cnf_file_list)
-            for i in range(min(N, length)):
-                cnf_file = cnf_file_list[i]
-                tool_file_command = tool_command + [cnf_file]
-                if DEBUG: print(" ".join(tool_file_command))
-                p = Popen(tool_file_command, stdout= PIPE, stderr=PIPE)
-                procdict[p.pid] = p
-            if len(procdict) == 0:
-                break
+            result = True
+            tool_command = tool_invocation.split(' ')
+            # parallel processes
+            # N = 16
             while True:
-                pid, _ = os.wait()
-                if pid in procdict.keys():
-                    res = procdict[pid].communicate()
-                    result = get_result(res[0], expected_prob, expected_abs_value)
-                    if result != True:
-                        break
-                    else:
-                        procdict.pop(pid)
+                procdict = {}
+                length = len(cnf_file_list)
+                for i in range(min(N, length)):
+                    cnf_file = cnf_file_list[i]
+                    tool_file_command = tool_command + [cnf_file]
+                    if DEBUG: print(" ".join(tool_file_command))
+                    p = Popen(tool_file_command, stdout= PIPE, stderr=PIPE)
+                    procdict[p.pid] = p
                 if len(procdict) == 0:
                     break
+                while True:
+                    pid, _ = os.wait()
+                    if pid in procdict.keys():
+                        res = procdict[pid].communicate()
+                        result = get_result(res[0], expected_prob, expected_abs_value)
+                        if result != True:
+                            break
+                        else:
+                            procdict.pop(pid)
+                    if len(procdict) == 0:
+                        break
 
-            if result != True:
-                break
+                if result != True:
+                    break
 
-            if length > N:
-                cnf_file_list = cnf_file_list[N: length]
-            else:
-                break
-
-        for pid in procdict.keys():
-            procdict[pid].kill()
-            procdict[pid].communicate()
-        
-        procdict = {}
-        signal.alarm(0)
-        return result
+                if length > N:
+                    cnf_file_list = cnf_file_list[N: length]
+                else:
+                    break
+            procdict = {}
+            return result
     except TimeoutException:
         return "TIMEOUT"
     except InvalidProcessNumException:
@@ -229,4 +199,3 @@ def CheckEquivalence(tool_invocation, cnf: 'CNF', cnf_file_root = tempfile.gette
             except Exception:
                 pass
         procdict = {}
-        signal.alarm(0)  # Cancel any pending alarms
