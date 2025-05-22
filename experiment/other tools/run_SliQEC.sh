ce93f91cc8d4f928429ae8a0188474aa9e4f8c4d
dekel zeldov <dekelzeldov@gmail.com>
2025-05-22 13:22:05 +0200

    cleanup
    Former-commit-id: b9daebca791e78bb5bd3d26f411b9fca2a2305bc [formerly ca35dd3b49a8b933da6be35d8510974bf6658999]
    Former-commit-id: 426bff5675f8a807a97b03f6582483d79ac9ef9e

--- experiment/other tools/run_SliQEC.sh
+++ experiment/other tools/run_SliQEC.sh
@@ -8,18 +8,6 @@
 
 export TIMEOUT=$3
 
-while read -r line; do
-    sed -i 's|rz(1.0\*pi)|z|g' '../benchmark/algorithm/origin/'$line'.qasm'
-    sed -i 's|rz(pi/2)|s|g' '../benchmark/algorithm/origin/'$line'.qasm'
-    sed -i 's|rx(0.5\*pi)|rx(pi/2)|g' '../benchmark/algorithm/origin/'$line'.qasm'
-    sed -i 's|rz(1.0\*pi)|z|g' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm'
-    sed -i 's|rz(pi/2)|s|g' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm'
-    sed -i 's|rx(0.5\*pi)|rx(pi/2)|g' '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm'
-    sed -i 's|rz(pi/2)|s|g' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm'
-    sed -i 's|rx(0.5\*pi)|rx(pi/2)|g' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm'
-    sed -i 's|rz(1.0\*pi)|z|g' '../benchmark/algorithm/gm/'$line'.qasm.gm.qasm'
-done < "$1"
-
 echo "new results origin vs opt:" >&1
 while read -r line; do
     ../../../SliQEC/SliQEC --circuit1 '../benchmark/algorithm/origin/'$line'.qasm' --circuit2 '../benchmark/algorithm/opt/'$line'.qasm.opt.qasm' >&1;
