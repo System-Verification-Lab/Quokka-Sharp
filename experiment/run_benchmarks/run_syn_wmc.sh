d90dfeac9deb8264c674b095660fdc0b5d748766
dekel zeldov <dekelzeldov@gmail.com>
2025-05-23 14:47:41 +0200

    testing cleanup

--- experiment/run_benchmarks/run_syn_wmc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script runs syn_run.py on the cs circuit.
-# arg1: file with all filepaths to run
-
-export TIMEOUT=$1
-
-python3 run_syn.py "../basic_tests/cs.qasm" -b "pauli" -f 1.0 >&1 ;
-python3 run_syn.py "../basic_tests/cs.qasm" -b "comp" -f 1.0 >&1 ;
