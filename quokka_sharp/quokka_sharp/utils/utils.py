1c89c2eb212eebc3c2459a9fcfca56f1177cfe64
dekel zeldov <dekelzeldov@gmail.com>
2025-05-23 14:23:14 +0200

    utils use FPR
    Former-commit-id: 14aef3e06f2a99f8f22f8ec106b37b577e46bf60 [formerly 75d07f62408123a70ceec581bae0b9588c7c74e3]
    Former-commit-id: 50d0712c2f56ce25577d292aaddc3e405767eb18

--- quokka_sharp/quokka_sharp/utils/utils.py
+++ quokka_sharp/quokka_sharp/utils/utils.py
@@ -6,23 +6,22 @@ from ..config import CONFIG
 tool_invocation = CONFIG["ToolInvocation"]
 get_result      = CONFIG["GetResult"]
 DEBUG           = CONFIG["DEBUG"]
+FPE             = CONFIG["FPE"]
 
 
 def parse_wmc_result(result, square: bool):
     """Parse the output of WMC to get the weighted model counting result."""
-    result = str(result)
-    ans_str = re.findall( re.compile(get_result), result)
+    ans_str = re.findall( re.compile(get_result), str(result))
     if DEBUG: print("weighted model counting result:", ans_str)
     if not ans_str:
         return -1
     ans_str = ans_str[0].replace("\\n", "").replace(" ", "").replace("i", "j").replace("+-", "-")
-    if DEBUG: print("weighted model counting result:", ans_str)
     ans = complex(ans_str)
     real, imag = Decimal(ans.real), Decimal(ans.imag)
 
-    if abs(real) < 1e-16 and abs(imag) < 1e-16:
+    if abs(real) < FPE and abs(imag) < FPE:
         return 0
-    elif abs(imag) < 1e-16:
+    elif abs(imag) < FPE:
         return real * real if square else real
     else:
         return (real * real + imag * imag) if square else (real * real + imag * imag).sqrt()
