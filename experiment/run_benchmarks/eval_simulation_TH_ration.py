7ae61c075fad126cdcae76d4ef3194a2c89bb908
dekel zeldov <dekelzeldov@gmail.com>
2025-05-28 20:01:14 +0200

    eval WIP

--- experiment/run_benchmarks/eval_simulation_TH_ration.py
+++ experiment/run_benchmarks/eval_simulation_TH_ration.py
@@ -10,9 +10,11 @@ benchmark_folder = os.path.join("random", "TH_ratio")
 benchmarks_list = utils.get_benchmark_files(benchmark_folder)
 
 results_file_name = "simulations_HT_rations.csv"
-df_columns = ["qubits", "deepth", "seed", "ratio", "measurement", "basis", "result", "time"]
+df_columns = ["qubits", "depth", "seed", "ratio", "measurement", "basis", "result", "time"]
 results_df = utils.get_results_from_file(results_file_name, df_columns)
 
+# Limits
+seed_limit = 10
 
 def get_from_file_name(file_name):
 	name = file_name.replace(".qasm", "")
@@ -20,14 +22,17 @@ def get_from_file_name(file_name):
 
 def draw_figures(results_df, results_file_name):
 	# check that for all entries in results_df, the coresponding result of the other basis is timeout or the same
-	for exp, results in results_df.groupby(["qubits", "deepth", "seed", "ratio", "measurement"]):
+	for exp, results in results_df.groupby(["qubits", "depth", "seed", "ratio", "measurement"]):
 		assert len(results) == 2, f"Expected 2 results for {exp}, but found {len(results)}\n{results}"
-
 		basis_results = results.set_index("basis")["result"]
-		if basis_results["comp"] == "TIMEOUT" or basis_results["pauli"] == "TIMEOUT":
+		if (basis_results["comp"] == "TIMEOUT") or (basis_results["pauli"] == "TIMEOUT"):
 			continue
-		if abs(float(basis_results["comp"]) - float(basis_results["pauli"])) > utils.FPE:
-			print(f"WARNING: Results mismatch for {exp}: {basis_results['comp']} vs {basis_results['pauli']}")
+		else:
+			if abs(float(basis_results["comp"]) - float(basis_results["pauli"])) > utils.FPE:
+				print(f"WARNING: Results mismatch for {exp}:\n{basis_results}")
+
+	# Filter results_df to only include entries with qubits <= qubits_limit and depth <= depth_limit
+	qubits_df = results_df[(results_df["seed"] <= seed_limit)]
 
 	# Assign a unique color for each basis combination
 	colors = utils.cycle_colors()
@@ -35,7 +40,7 @@ def draw_figures(results_df, results_file_name):
 
 	# Assign a unique line style for each depth
 	lines = utils.line_style_cycle()
-	line_map = {d: next(lines) for d in sorted(results_df["deepth"].unique())}
+	line_map = {d: next(lines) for d in sorted(results_df["depth"].unique())}
 
 	for qubits in results_df["qubits"].unique():
 		qubits_df = results_df[results_df["qubits"] == qubits]
@@ -43,7 +48,7 @@ def draw_figures(results_df, results_file_name):
 			continue
 
 		plt.figure(figsize=(10, 6))
-		for (deepth, basis), group in qubits_df.groupby(["deepth", "basis"]):
+		for (depth, basis), group in qubits_df.groupby(["depth", "basis"]):
 			# Calculate the mean and std time for each group, excluding TIMEOUT
 			group = group[group["result"] != "TIMEOUT"]
 			if group.empty:
@@ -54,11 +59,11 @@ def draw_figures(results_df, results_file_name):
 				group["ratio"],
 				group["time"]["mean"],
 				yerr=group["time"]["std"],
-				label=f"deepth={deepth}, {basis}",
+				label=f"depth={depth}, {basis}",
 				fmt='o',
 				capsize=5,
 				color=color_map[basis],
-				linestyle=line_map[deepth]
+				linestyle=line_map[depth]
 			)
 		plt.title(f"Mean Time vs Ratio for Simulation of {qubits} Qubits")
 		plt.xlabel("T gates to H gates Ratio")
@@ -71,14 +76,17 @@ def draw_figures(results_df, results_file_name):
 
 for file in tqdm(benchmarks_list, desc="Processing files", unit="file"):
 	file_path = utils.get_file_path(file, "origin", benchmark_folder)
-	qubits, deepth, seed, ratio = get_from_file_name(file)
+	qubits, depth, seed, ratio = get_from_file_name(file)
 	ratio = float(ratio) / 10  # Convert to float from string representation
 	new = False
+	# Skip if the seed is greater than the limit
+	if seed > seed_limit:
+		continue
 	for measurement in ["allzero"]:
 		for basis in ["comp", "pauli"]:
 			run_data = {
 				"qubits": qubits,
-				"deepth": deepth,
+				"depth": depth,
 				"seed": seed,
 				"ratio": ratio,
 				"measurement": measurement,
@@ -98,10 +106,11 @@ for file in tqdm(benchmarks_list, desc="Processing files", unit="file"):
 			new = True
 
 	if new:
+		utils.save_results_to_file(results_file_name, results_df)
 		draw_figures(results_df, results_file_name)
 
 # Save the results to a file
-results_df.sort_values(by=["qubits", "deepth", "seed", "ratio", "measurement", "basis"], inplace=True)
+results_df.sort_values(by=["qubits", "depth", "seed", "ratio", "measurement", "basis"], inplace=True)
 utils.save_results_to_file(results_file_name, results_df)
 draw_figures(results_df, results_file_name)
 print(results_df)
