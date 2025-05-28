89267c5020c56dd8bb25a7006250b79046586aa8
dekel zeldov <dekelzeldov@gmail.com>
2025-05-28 13:35:15 +0200

    set config path with a func instead of with a global param

--- quokka_sharp/quokka_sharp/config.py
+++ quokka_sharp/quokka_sharp/config.py
@@ -12,9 +12,19 @@ DEFAULT_CONFIG = {
     "Precision" : 32
 }
 
+_config_path = None
+
+def set_config_path(path):
+    """
+    Set the path to the config file. Call this before using any Quokka functionality if you want to override the default config.
+    Automatically reloads CONFIG.
+    """
+    global _config_path, CONFIG
+    _config_path = path
+    CONFIG = load_config()
+
 def load_config():
-    config_path = os.getenv("QUOKKA_CONFIG", None)
-    
+    config_path = _config_path
     if config_path and os.path.isfile(config_path):
         try:
             with open(config_path, "r") as f:
@@ -22,7 +32,6 @@ def load_config():
                 return {**DEFAULT_CONFIG, **user_config}
         except Exception as e:
             raise RuntimeError(f"Failed to load config file: {e}")
-    
     return DEFAULT_CONFIG
 
 CONFIG = load_config()
