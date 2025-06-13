7cce0548ef229abce4a51ae02505a3c5bf551faf
dekel zeldov <dekelzeldov@gmail.com>
2025-06-13 16:59:38 +0200

    verification remove tool path + func documentation
    Former-commit-id: bb299a128d7a62da6e4bfcf891b44cbfd39527c1 [formerly 735b932b0ca5860d623405dd8b28ef63de4ce0ab]
    Former-commit-id: c818f021db1c1e0571e91c1edc5386c9f16926ab

--- quokka_sharp/quokka_sharp/functionalities.py
+++ quokka_sharp/quokka_sharp/functionalities.py
@@ -2,6 +2,15 @@ import quokka_sharp as qk
 
 class functionalities:
 	def eq(qasmfile1, qasmfile2, basis, check, N = 1): 
+		"""
+		Compare two quantum circuits given in QASM format.
+		:param qasmfile1: Path to the first QASM file.
+		:param qasmfile2: Path to the second QASM file.
+		:param basis: The basis to use for the comparison, either "comp" for computational basis or "pauli" for Pauli basis.
+		:param check: The type of check to use, either "cyclic", "linear", or "cyclic_linear".
+		:param N: The number of parallel calles to the WMC, relevant only if check is "linear".
+		:return: Boolean indicating whether the two circuits are equivalent.
+		"""
 		# Parse the circuit
 		circuit1 = qk.encoding.QASMparser(qasmfile1, True)
 		# Parse another circuit
@@ -14,27 +23,55 @@ class functionalities:
 		# "id" or "2n"
 		return qk.CheckEquivalence(cnf, check = check, N = N)
 	
-	def sim(qasmfile, basis, meas): 
+	def sim(qasmfile, basis, measurement): 
+		"""
+		Simulate a quantum circuit given in QASM format.
+		:param qasmfile: Path to the QASM file.
+		:param basis: The basis to use for the simulation, either "comp" for computational basis or "pauli" for Pauli basis.
+		:param measurement: The type of measurement to compute the probability for, either "allzero", "firstzero", or a dictionary with qubit indices and their expected values.
+		:return: Simulation result, a float representing the probability of the measurement outcome.
+		"""
 		# Parse the circuit
 		circuit1 = qk.encoding.QASMparser(qasmfile, True)
 		# Encode the circuit
 		cnf = qk.encoding.QASM2CNF(circuit1, computational_basis = (basis == "comp"), weighted=True)
 		cnf.leftProjectAllZero()
-		cnf.add_measurement(meas)
+		cnf.add_measurement(measurement)
 		return qk.Simulate(cnf)
 	
 	def syn(qasmfile, basis, cyc_lin_encoding=False, fid=1, files_root=None): 
+		"""
+		Synthesize a quantum circuit given in QASM format.
+		:param qasmfile: Path to the QASM file.
+		:param basis: The basis to use for the synthesis, either "comp" for computational basis or "pauli" for Pauli basis.
+		:param cyc_lin_encoding: Whether to use the cyclic linear encoding, relevant only for the Pauli basis.
+		:param fid: Fidelity threshold for the synthesis.
+		:param files_root: Root directory for the cnf and MWMC output files.
+		:return: A tuple with four elements:
+			- outcome(str): The outcome of the synthesis, which can be "FOUND", "TIMEOUT", or "CRASH".
+			- weight(float): The weight of the best synthesized circuit.
+			- qasm(str): A QASM representation of the synthesized circuit.
+			- layers(int): The number of layers in the synthesized circuit.
+		"""
 		circuit = qk.encoding.QASMparser(qasmfile, True)
 		circuit.dagger()
 		cnf = qk.encoding.QASM2CNF(circuit, computational_basis = (basis == "comp"))
 
 		return qk.Synthesis(cnf, onehot_xz = cyc_lin_encoding, fidelity_threshold = fid, cnf_file_root=files_root)
         
-	def veri(qasmfile, basis): 
+	def veri(qasmfile, basis, precons, postcons): 
+		"""
+		Verify a quantum circuit given in QASM format against preconditions and postconditions.
+		:param qasmfile: Path to the QASM file.
+		:param basis: The basis to use for the verification, either "comp" for computational basis or "pauli" for Pauli basis.
+		:param precons: A list of preconditions to check before the circuit execution.
+		:param postcons: A list of postconditions to check after the circuit execution.
+		:return: Verification result, which can be "True", "False", or "TIMEOUT".
+		"""
 		# Parse the circuit
 		circuit = qk.encoding.QASMparser(qasmfile, True)
 		# Get CNF for the merged circuit (for computational base instaed of cliffordt, use `computational_basis = True`)
 		cnf = qk.encoding.QASM2CNF(circuit, computational_basis = (basis == "comp"))
 		# "id" or "2n"
-		res = qk.Verify(cnf)
+		res = qk.Verify(cnf, precons, postcons)
 		return res
