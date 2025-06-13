735b932b0ca5860d623405dd8b28ef63de4ce0ab
dekel zeldov <dekelzeldov@gmail.com>
2025-06-13 16:59:38 +0200

    verification remove tool path + func documentation

--- quokka_sharp/quokka_sharp/veri.py
+++ quokka_sharp/quokka_sharp/veri.py
@@ -2,11 +2,10 @@ from .encoding.cnf import CNF
 from .sim import Simulate
 from .sim import FPE
 
-def Verify(tool_path, cnf: "CNF", precons, postcons):
+def Verify(cnf: "CNF", precons, postcons):
     """
     Function to verify a circuit with a pre-condition and postcondition
     Args:
-        tool_path   :  the running command of the weighted model counter
         cnf         :  the encoded cnf of the input circuit
         precons     :  a dictionary specifying the pre-condition. The keys are qubit indices and the values are 0 or 1.
         postcons    :  a dictionary specifying the post-condition. The keys are qubit indices and the values are 0 or 1.
@@ -15,7 +14,7 @@ def Verify(tool_path, cnf: "CNF", precons, postcons):
     """
     cnf.precondition(precons)
     cnf.postcondition(postcons)
-    prob = Simulate(tool_path, cnf)
+    prob = Simulate(cnf)
     if type(prob) is str:
         return prob
     if abs(prob - 1) < FPE:
