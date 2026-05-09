5a899860b72096ef3ce8bbb0b49fb3a0fb7a0b35
JingyiMei98 <j.mei@liacs.leidenuniv.nl>
2026-05-09 13:14:05 +0200

    version=2.7.4

--- quokka_sharp/setup.py
+++ quokka_sharp/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2.7.3'
+VERSION = '2.7.4'
 DESCRIPTION = 'Quokka Sharp'
 LONG_DESCRIPTION = '''
                     Quokka Sharp for simulating and equivalence checking 
